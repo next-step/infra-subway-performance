66f7e21ac9f21de7dd0e0e525dc6edacb3330490
김우재 <kwj1270@naver.com>
2021-06-19 17:02:43 +0900

    feat:SubWayVersion 빈이 현재 버전을 반환하는지 기능 구현

--- src/main/java/nextstep/subway/config/support/SubWayVersion.java
+++ src/main/java/nextstep/subway/config/support/SubWayVersion.java
@@ -8,5 +8,22 @@ import java.time.format.DateTimeFormatter;
 
 @Component
 public class SubWayVersion {
+    private static final String DEFAULT_DATE_TIME_FORMAT = "yyyyMMddHHmmss";
+
+    private String version;
+
+    @PostConstruct
+    public void init() {
+        version = nowVersion();
+    }
+
+    public static String nowVersion() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(DEFAULT_DATE_TIME_FORMAT);
+        return LocalDateTime.now().format(formatter);
+    }
+
+    public String version() {
+        return version;
+    }
 
 }
