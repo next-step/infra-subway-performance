7bb431ef49f94c107e6663ed3c9bdb083b9905ec
jay kim <jaykim@jays-MacBook-Pro.local>
2021-06-23 14:27:08 +0900

    feat(cache): add redis config

--- frontend/webpack.prod.js
+++ frontend/webpack.prod.js
@@ -1,9 +1,13 @@
 const path = require('path')
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 const outputPath = path.resolve(__dirname, '../src/main/resources/static')
 
 module.exports = {
   mode: 'production',
+  plugins : [
+      new BundleAnalyzerPlugin()
+  ],
   output: {
     path: outputPath,
     filename: '[name].js'
