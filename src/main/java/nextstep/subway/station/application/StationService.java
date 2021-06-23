e34ee06b262bb9ee288c5808ba411e88141a0349
jay kim <jaykim@jays-MacBook-Pro.local>
2021-06-23 23:28:26 +0900

    feat: add more redis cache

--- src/main/java/nextstep/subway/station/application/StationService.java
+++ src/main/java/nextstep/subway/station/application/StationService.java
@@ -6,6 +6,8 @@ import nextstep.subway.station.dto.StationRequest;
 import nextstep.subway.station.dto.StationResponse;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.Caching;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -21,11 +23,13 @@ public class StationService {
         this.stationRepository = stationRepository;
     }
 
+    @CacheEvict(value="stations")
     public StationResponse saveStation(StationRequest stationRequest) {
         Station persistStation = stationRepository.save(stationRequest.toStation());
         return StationResponse.of(persistStation);
     }
 
+    @Cacheable(value="stations")
     @Transactional(readOnly = true)
     public List<StationResponse> findAllStations() {
         List<Station> stations = stationRepository.findAll();
@@ -35,16 +39,17 @@ public class StationService {
                 .collect(Collectors.toList());
     }
 
-    @CacheEvict(value="station", key="#id")
+    @Caching(evict = {@CacheEvict(value="stations"), @CacheEvict(value="station", key="#id")})
     public void deleteStationById(Long id) {
         stationRepository.deleteById(id);
     }
 
-    @CachePut(value="station", key="#id")
+    @Cacheable(value="station", key="#id")
     public Station findStationById(Long id) {
         return stationRepository.findById(id).orElseThrow(RuntimeException::new);
     }
 
+    @Cacheable(value="station", key="#id")
     public Station findById(Long id) {
         return stationRepository.findById(id).orElseThrow(RuntimeException::new);
     }
