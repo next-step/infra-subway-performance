3202a7d345ef2bda4e950fe02d276397580f4e4f
김우재 <kwj1270@naver.com>
2021-06-24 02:36:53 +0900

    feat:Map 테스트 작성

--- src/main/java/nextstep/subway/config/CacheConfig.java
+++ src/main/java/nextstep/subway/config/CacheConfig.java
@@ -1,5 +1,6 @@
 package nextstep.subway.config;
 
+import nextstep.subway.map.dto.PathResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.CachingConfigurerSupport;
@@ -10,6 +11,8 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
@@ -17,12 +20,11 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @EnableCaching
 @Configuration
-@Profile("prod")
 public class CacheConfig extends CachingConfigurerSupport {
 
     @Autowired
     RedisConnectionFactory redisConnectionFactory;
-
+    
     @Bean
     public CacheManager redisCacheManager() {
         RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
@@ -32,6 +34,21 @@ public class CacheConfig extends CachingConfigurerSupport {
         RedisCacheManager redisCacheManager = RedisCacheManager.RedisCacheManagerBuilder
                 .fromConnectionFactory(redisConnectionFactory).cacheDefaults(redisCacheConfiguration).build();
         return redisCacheManager;
+    }
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        LettuceConnectionFactory lettuceConnectionFactory = new LettuceConnectionFactory();
+        return lettuceConnectionFactory;
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate() {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory());
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new CustomJackson2JsonRedisSerializer(PathResponse.class));
+        return redisTemplate;
     }
 
 }
