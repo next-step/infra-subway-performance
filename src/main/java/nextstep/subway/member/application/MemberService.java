c99eb4bcb2a92ac3cd2e7f3787694af48c5a5f92
prodo-developer <wjdals300@gmail.com>
2021-07-03 21:38:53 +0900

    feat : Spring Data Cache 처리

--- src/main/java/nextstep/subway/member/application/MemberService.java
+++ src/main/java/nextstep/subway/member/application/MemberService.java
@@ -4,6 +4,7 @@ import nextstep.subway.member.domain.Member;
 import nextstep.subway.member.domain.MemberRepository;
 import nextstep.subway.member.dto.MemberRequest;
 import nextstep.subway.member.dto.MemberResponse;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -16,16 +17,19 @@ public class MemberService {
         this.memberRepository = memberRepository;
     }
 
+    @Cacheable(value="member", key="#id")
     public MemberResponse createMember(MemberRequest request) {
         Member member = memberRepository.save(request.toMember());
         return MemberResponse.of(member);
     }
 
+    @Cacheable(value="member", key="#id")
     public MemberResponse findMember(Long id) {
         Member member = memberRepository.findById(id).orElseThrow(RuntimeException::new);
         return MemberResponse.of(member);
     }
 
+    @Cacheable(value="member", key="#id")
     public void updateMember(Long id, MemberRequest param) {
         Member member = memberRepository.findById(id).orElseThrow(RuntimeException::new);
         member.update(param.toMember());
