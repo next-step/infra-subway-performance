08051334e758a61226688d6a1f712c898b19edea
HoonHee Jeong <jhh992000@gmail.com>
2021-07-06 17:13:20 +0900

    feat: K6 성능테스트 스크립트 추가

---
+++ k6/update-data/smoke.js
@@ -0,0 +1,56 @@
+import http from 'k6/http';
+import { check, group, sleep, fail } from 'k6';
+
+export let options = {
+  vus: 1, // 1 user looping for 1 minute
+  duration: '10s',
+
+  thresholds: {
+    http_req_duration: ['p(99)<1500'], // 99% of requests must complete below 1.5s
+  },
+};
+
+const BASE_URL = 'https://jhh992000.ddns.net';
+
+export default function ()  {
+  let 메인페이지_접속_결과 = 메인페이지_접속_요청();
+  메인페이지_접속_결과_확인(메인페이지_접속_결과);
+
+  let 회원가입_결과 = 회원가입_요청();
+  회원가입_결과_확인(회원가입_결과);
+};
+
+export function 메인페이지_접속_요청() {
+  return http.get(`${BASE_URL}`);
+}
+export function 메인페이지_접속_결과_확인(메인페이지_접속_결과) {
+  check(메인페이지_접속_결과, {
+    '메인페이지가 정상적으로 응답함': (response) => response.status === 200
+  });
+}
+export function 회원가입_요청() {
+  var payload = JSON.stringify({
+    email: guid() + "@gmail.com",
+    age: "99",
+    password: "1234"
+  });
+
+  var params = {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
+
+  return http.post(`${BASE_URL}/members`, payload, params);
+}
+export function 회원가입_결과_확인(회원가입_결과) {
+  check(회원가입_결과, {
+    '회원가입이 정상적으로 처리됨': (response) => response.status === 201
+  });
+}
+export function guid() {
+  function s4() {
+    return ((1 + Math.random()) * 0x10000 | 0).toString(16).substring(1);
+  }
+  return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
+}
---
+++ k6/update-data/stress.js
@@ -0,0 +1,63 @@
+import http from 'k6/http';
+import { check, group, sleep, fail } from 'k6';
+
+export let options = {
+  stages: [
+    { duration: '5s', target: 50 },
+    { duration: '10s', target: 200 },
+    { duration: '10s', target: 400 },
+    { duration: '10s', target: 300 },
+    { duration: '10s', target: 500 },
+    { duration: '5s', target: 200 },
+    { duration: '5s', target: 50 },
+  ],
+
+  thresholds: {
+    http_req_duration: ['p(99)<1500'], // 99% of requests must complete below 1.5s
+  },
+};
+
+const BASE_URL = 'https://jhh992000.ddns.net';
+
+export default function ()  {
+  let 메인페이지_접속_결과 = 메인페이지_접속_요청();
+  메인페이지_접속_결과_확인(메인페이지_접속_결과);
+
+  let 회원가입_결과 = 회원가입_요청();
+  회원가입_결과_확인(회원가입_결과);
+};
+
+export function 메인페이지_접속_요청() {
+  return http.get(`${BASE_URL}`);
+}
+export function 메인페이지_접속_결과_확인(메인페이지_접속_결과) {
+  check(메인페이지_접속_결과, {
+    '메인페이지가 정상적으로 응답함': (response) => response.status === 200
+  });
+}
+export function 회원가입_요청() {
+  var payload = JSON.stringify({
+    email: guid() + "@gmail.com",
+    age: "99",
+    password: "1234"
+  });
+
+  var params = {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
+
+  return http.post(`${BASE_URL}/members`, payload, params);
+}
+export function 회원가입_결과_확인(회원가입_결과) {
+  check(회원가입_결과, {
+    '회원가입이 정상적으로 처리됨': (response) => response.status === 201
+  });
+}
+export function guid() {
+  function s4() {
+    return ((1 + Math.random()) * 0x10000 | 0).toString(16).substring(1);
+  }
+  return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
+}
