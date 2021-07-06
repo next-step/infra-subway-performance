0db745b071291948d9f5823fd41ceaa039e02361
dhmin5693 <dhmin5693@naver.com>
2021-07-07 03:21:48 +0900

    feat: 페이징 쿼리로 변경

--- src/main/java/nextstep/subway/favorite/domain/FavoriteRepository.java
+++ src/main/java/nextstep/subway/favorite/domain/FavoriteRepository.java
@@ -1,9 +1,8 @@
 package nextstep.subway.favorite.domain;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface FavoriteRepository extends JpaRepository<Favorite, Long> {
-    List<Favorite> findByMemberId(Long memberId);
+    List<Favorite> findTop5ByMemberIdOrderByCreatedDateDesc(Long memberId);
 }
