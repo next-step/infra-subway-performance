7e2940b767c8e8d50acc6717231e16b119950ba5
So Jeong Park <psj6414@naver.com>
2021-07-07 21:29:20 +0900

    [Step1] 리뷰 요청 드립니다. (#16)
    * feat: application properties 관련 파일 및 build.gradle 추가
    
    * feat: cache 관련 설정파일 추가
    
    * feat: 캐시 적용
    
    * docs: k6 javascript 및 부하 테스트 결과 추가
    
    * docs : step1 답변 작성
    
    * fix: login 부하 테스트 js 코드 수정 및 테스트 재실행 결과 추가
    
    * fix: main 최대 부하 스트레스 테스트 결과 추가

--- README.md
+++ README.md
@@ -43,9 +43,45 @@ npm run dev
 
 ### 1단계 - 화면 응답 개선하기
 1. 성능 개선 결과를 공유해주세요 (Smoke, Load, Stress 테스트 결과)
+- [x] 대상 페이지
+    * 메인, 로그인, 경로 검색
+- [x] 부하 테스트 시 저장될 데이터 건수 및 크기  
+    * 준비 된 운영 DB 데이터  
+- [x] 스크립트 작성
+    * loadTest/longin 
+        * smoke.js
+        * load.js
+        * stress.js
+        
+    * loadTest/main
+        * smoke.js
+        * load.js
+        * stress.js
 
+    * loadTest/path
+        * smoke.js
+        * load.js
+        * stress.js
+
+- [x] 개선 전 테스트 결과
+    * loadTest/longin/beforeResult.txt
+    * loadTest/main/beforeResult.txt
+    * loadTest/path/beforeResult.txt
+
+- [x] 개선 후 테스트 결과
+    * loadTest/longin/afterResult.txt
+    * loadTest/main/afterResult.txt
+    * loadTest/path/afterResult.txt
+    
 2. 어떤 부분을 개선해보셨나요? 과정을 설명해주세요
-
+    - [x] Reverse Proxy 개선
+        * gzip 압축 활성화
+        * HTTP2.0 설정
+    - [x] WAS 성능 개선
+        * redis 적용
+            * application-prod.properties 에 redis 설정값 추가 및 build.gradle 에 redis 추가
+            * DB 서버에 Docker로 Redis Server 기동
+            * 조회가 빈번한 station service, map service, member service 관련 메소드에 캐시 적용
 ---
 
 ### 2단계 - 조회 성능 개선하기
---
+++ src/main/java/nextstep/subway/common/CustomRedisSerializer.java
@@ -0,0 +1,58 @@
+package nextstep.subway.common;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.JavaType;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalTimeSerializer;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+public class CustomRedisSerializer extends Jackson2JsonRedisSerializer {
+
+    private static final String    DEFAULT_DATE_TIME_FORMAT = "yyyyMMddHHmmss";
+    private static final String    DEFAULT_DATE_FORMAT      = "yyyyMMdd";
+    private static final String    DEFAULT_TIME_FORMAT      = "HHmmss";
+
+    public CustomRedisSerializer(Class type) {
+        super(type);
+        setLocalDateMapper();
+    }
+
+    public CustomRedisSerializer(JavaType javaType) {
+        super(javaType);
+        setLocalDateMapper();
+    }
+
+    private void setLocalDateMapper()  {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        objectMapper.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+
+        JavaTimeModule javaTimeModule = new JavaTimeModule();
+        javaTimeModule.addSerializer(LocalDateTime.class, new LocalDateTimeSerializer(
+                DateTimeFormatter.ofPattern(DEFAULT_DATE_TIME_FORMAT)));
+        javaTimeModule.addSerializer(LocalDate.class,
+                new LocalDateSerializer(DateTimeFormatter.ofPattern(DEFAULT_DATE_FORMAT)));
+        javaTimeModule.addSerializer(LocalTime.class,
+                new LocalTimeSerializer(DateTimeFormatter.ofPattern(DEFAULT_TIME_FORMAT)));
+        javaTimeModule.addDeserializer(LocalDateTime.class, new LocalDateTimeDeserializer(
+                DateTimeFormatter.ofPattern(DEFAULT_DATE_TIME_FORMAT)));
+        javaTimeModule.addDeserializer(LocalDate.class,
+                new LocalDateDeserializer(DateTimeFormatter.ofPattern(DEFAULT_DATE_FORMAT)));
+        javaTimeModule.addDeserializer(LocalTime.class,
+                new LocalTimeDeserializer(DateTimeFormatter.ofPattern(DEFAULT_TIME_FORMAT)));
+        objectMapper.registerModule(javaTimeModule);
+
+        setObjectMapper(objectMapper);
+    }
+}
