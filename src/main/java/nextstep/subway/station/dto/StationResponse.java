19f6bddbcebcd4e182845fdb2e44e9aac655f462
youngjun.jin <yzzzzun@gmail.com>
2021-07-09 00:36:46 +0900

    refactor: redis json파싱 오류 수정

--- src/main/java/nextstep/subway/station/dto/StationResponse.java
+++ src/main/java/nextstep/subway/station/dto/StationResponse.java
@@ -1,13 +1,24 @@
 package nextstep.subway.station.dto;
 
+import jdk.nashorn.internal.ir.annotations.Ignore;
 import nextstep.subway.station.domain.Station;
 
 import java.time.LocalDateTime;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+
 public class StationResponse {
     private Long id;
     private String name;
+    @JsonIgnore
     private LocalDateTime createdDate;
+    @JsonIgnore
     private LocalDateTime modifiedDate;
 
     public static StationResponse of(Station station) {
