63ef3dc32426a3d5faddcab05bc1a5ec7f708ec2
gin <gin@smallmachines.co.kr>
2021-07-08 15:20:54 +0900

    feat: Async 어노테이션의 thread config 클래스 추가

---
+++ src/main/java/nextstep/subway/config/AsyncThreadConfig.java
@@ -0,0 +1,32 @@
+package nextstep.subway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@Configuration
+@EnableAsync
+public class AsyncThreadConfig {
+
+    @Bean
+    public Executor asyncThreadTaskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        /* MaxThread가 동작하는 경우 대기하는 Queue 사이즈 */
+        executor.setQueueCapacity(100);
+        /* 기본 Thread 사이즈 */
+        executor.setCorePoolSize(2);
+        /* 최대 Thread 사이즈 */
+        executor.setMaxPoolSize(4);
+
+        executor.setAllowCoreThreadTimeOut(true);
+
+        executor.setKeepAliveSeconds(60);
+
+        executor.setThreadNamePrefix("subway-async-");
+
+        return executor;
+    }
+}
