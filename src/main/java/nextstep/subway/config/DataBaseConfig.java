014c5f81b56c8e2e18c278e6f90bd31e5786bcbb
lkimilhol <lkimilhol@gmail.com>
2021-07-09 00:26:57 +0900

    feat(*.*): 설정 수정

--- src/main/java/nextstep/subway/config/DataBaseConfig.java
+++ src/main/java/nextstep/subway/config/DataBaseConfig.java
@@ -9,7 +9,6 @@ import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
-import org.springframework.context.annotation.Profile;
 import org.springframework.jdbc.datasource.LazyConnectionDataSourceProxy;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
@@ -22,7 +21,6 @@ import static nextstep.subway.config.ReplicationRoutingDataSource.DATASOURCE_KEY
 @Configuration
 @EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
 @EnableTransactionManagement
-@Profile("prod")
 class DataBaseConfig {
 
     @Bean
