b3fd25c241ac47c5bb532a29fd9ac086c2a5f4a6
HoonHee Jeong <jhh992000@gmail.com>
2021-07-09 01:09:44 +0900

    feat: 배포스크립트 변경 (was5대로 증설)

--- deploy-script/proxy/web4-down.sh
+++ deploy-script/proxy/web4-down.sh
@@ -3,7 +3,7 @@
 sudo echo -e "least_conn;" > /etc/nginx/upstream.conf
 sudo echo -e "server 192.168.100.157:8080 max_fails=3 fail_timeout=3s;" >> /etc/nginx/upstream.conf
 sudo echo -e "server 192.168.100.59:8080 max_fails=3 fail_timeout=3s;" >> /etc/nginx/upstream.conf
-sudo echo -e "server 192.168.100.39:8080 down;" >> /etc/nginx/upstream.conf
-sudo echo -e "server 192.168.100.37:8080 max_fails=3 fail_timeout=3s;" >> /etc/nginx/upstream.conf
+sudo echo -e "server 192.168.100.39:8080 max_fails=3 fail_timeout=3s;" >> /etc/nginx/upstream.conf
+sudo echo -e "server 192.168.100.37:8080 down;" >> /etc/nginx/upstream.conf
 sudo echo -e "server 192.168.100.60:8080 max_fails=3 fail_timeout=3s;" >> /etc/nginx/upstream.conf
 sudo nginx -s reload
