146200c927d50665db40a632f584f020e7efd552
prodo-developer <wjdals300@gmail.com>
2021-07-10 23:51:14 +0900

    refactor : @Profile 수정

--- src/main/java/nextstep/subway/common/AuthenticationPrincipalConfig.java
+++ src/main/java/nextstep/subway/common/AuthenticationPrincipalConfig.java
@@ -5,13 +5,11 @@ import nextstep.subway.auth.ui.AuthenticationPrincipalArgumentResolver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
 
 @Configuration
-@Profile("prod")
 @ComponentScan({"nextstep.subway.auth.application"})
 public class AuthenticationPrincipalConfig implements WebMvcConfigurer {
     private final AuthService authService;
