92a9e011c7d460f54066fb46a6b729f62c3996bf
Jiseon Lee <leejoyc9310@gmail.com>
2021-07-11 20:31:04 +0900

    feat : 즐겨찾기 Pageable 개선

--- src/main/java/nextstep/subway/favorite/application/FavoriteService.java
+++ src/main/java/nextstep/subway/favorite/application/FavoriteService.java
@@ -9,6 +9,8 @@ import nextstep.subway.favorite.dto.FavoriteResponse;
 import nextstep.subway.station.domain.Station;
 import nextstep.subway.station.domain.StationRepository;
 import nextstep.subway.station.dto.StationResponse;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
@@ -34,16 +36,16 @@ public class FavoriteService {
         favoriteRepository.save(favorite);
     }
 
-    public List<FavoriteResponse> findFavorites(LoginMember loginMember, Pageable pageable) {
-        List<Favorite> favorites = favoriteRepository.findByMemberId(loginMember.getId(), pageable);
+    public Page<FavoriteResponse> findFavorites(LoginMember loginMember, Pageable pageable) {
+        Page<Favorite> favorites = favoriteRepository.findByMemberId(loginMember.getId(), pageable);
         Map<Long, Station> stations = extractStations(favorites);
-
-        return favorites.stream()
+        List<FavoriteResponse> favoriteResponses = favorites.stream()
                 .map(it -> FavoriteResponse.of(
                         it,
                         StationResponse.of(stations.get(it.getSourceStationId())),
                         StationResponse.of(stations.get(it.getTargetStationId()))))
                 .collect(Collectors.toList());
+        return new PageImpl<>(favoriteResponses, favorites.getPageable(), favorites.getTotalElements());
     }
 
     public void deleteFavorite(LoginMember loginMember, Long id) {
@@ -54,13 +56,13 @@ public class FavoriteService {
         favoriteRepository.deleteById(id);
     }
 
-    private Map<Long, Station> extractStations(List<Favorite> favorites) {
+    private Map<Long, Station> extractStations(Page<Favorite> favorites) {
         Set<Long> stationIds = extractStationIds(favorites);
         return stationRepository.findAllById(stationIds).stream()
                 .collect(Collectors.toMap(Station::getId, Function.identity()));
     }
 
-    private Set<Long> extractStationIds(List<Favorite> favorites) {
+    private Set<Long> extractStationIds(Page<Favorite> favorites) {
         Set<Long> stationIds = new HashSet<>();
         for (Favorite favorite : favorites) {
             stationIds.add(favorite.getSourceStationId());
