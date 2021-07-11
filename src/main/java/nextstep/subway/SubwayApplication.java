68155a6fdacc8b06d7c1986193594c78eb51a0da
박소정 <psj6414@naver.com>
2021-07-11 15:43:11 +0900

    feat: 데이터베이스 이중화 설정 파일 추가

--- src/main/java/nextstep/subway/SubwayApplication.java
+++ src/main/java/nextstep/subway/SubwayApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@EnableJpaRepositories
+
 @EnableJpaAuditing
 @SpringBootApplication
 public class SubwayApplication {
