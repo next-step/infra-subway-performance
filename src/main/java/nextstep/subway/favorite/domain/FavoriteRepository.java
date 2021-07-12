4f82655dbaf27194437dd34be1292cdd034fe2b2
Jun <p_illusion@naver.com>
2021-07-12 17:07:27 +0900

    refactor: 즐겨찾기 페이징 기능 추가

--- src/main/java/nextstep/subway/favorite/domain/FavoriteRepository.java
+++ src/main/java/nextstep/subway/favorite/domain/FavoriteRepository.java
@@ -1,9 +1,10 @@
 package nextstep.subway.favorite.domain;
 
+import java.util.List;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface FavoriteRepository extends JpaRepository<Favorite, Long> {
-    List<Favorite> findByMemberId(Long memberId);
+
+    List<Favorite> findByMemberId(Long memberId, Pageable pageable);
 }
