da7275bac996ab0521e447903ea5541852e41422
5minho <5minhominho@gmail.com>
2021-07-13 22:21:25 +0900

    feat: redis cache 를 사용해서 지하철역과, 최단경로를 캐시한다.

--- src/main/java/nextstep/subway/station/application/StationService.java
+++ src/main/java/nextstep/subway/station/application/StationService.java
@@ -4,6 +4,9 @@ import nextstep.subway.station.domain.Station;
 import nextstep.subway.station.domain.StationRepository;
 import nextstep.subway.station.dto.StationRequest;
 import nextstep.subway.station.dto.StationResponse;
+
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,17 +16,21 @@ import java.util.stream.Collectors;
 @Service
 @Transactional
 public class StationService {
+    public static final String ALL_STATIONS_CACHE_KEY = "STATIONS";
+
     private StationRepository stationRepository;
 
     public StationService(StationRepository stationRepository) {
         this.stationRepository = stationRepository;
     }
 
+    @CacheEvict(value = "stations", key = "#root.target.ALL_STATIONS_CACHE_KEY")
     public StationResponse saveStation(StationRequest stationRequest) {
         Station persistStation = stationRepository.save(stationRequest.toStation());
         return StationResponse.of(persistStation);
     }
 
+    @Cacheable(value = "stations", key = "#root.target.ALL_STATIONS_CACHE_KEY")
     @Transactional(readOnly = true)
     public List<StationResponse> findAllStations() {
         List<Station> stations = stationRepository.findAll();
@@ -33,6 +40,7 @@ public class StationService {
                 .collect(Collectors.toList());
     }
 
+    @CacheEvict(value = "stations", key = "#root.target.ALL_STATIONS_CACHE_KEY")
     public void deleteStationById(Long id) {
         stationRepository.deleteById(id);
     }
