827aea6a755c15fc7feab83d31fb86b0665ab746
seokjin kim <35091408+7271kim@users.noreply.github.com>
2021-07-13 17:39:00 +0900

    [STEP1] 1단계 - 화면 응답 개선하기 (#35)
    * [STEP1] 사전 테스트 결과 추가
    
    * [STEP1] Redis 추가
    
    * fix : [SETP1] findStationById, findById 호출을 addLineStation 에서 호출하여 이슈 발생
    제거진행
    
    * fix : [STEP1] "REDIS_KEY" > REDIS_KEY 수정
    
    * fix : [STEP1] 개선 가능한 항목 재 정리 및 전후 비교 수정 추가
    
    * docs : [STEP1] ReadMe 작성

--- src/main/java/nextstep/subway/line/dto/LineResponse.java
+++ src/main/java/nextstep/subway/line/dto/LineResponse.java
@@ -1,25 +1,36 @@
 package nextstep.subway.line.dto;
 
-import nextstep.subway.line.domain.Line;
-import nextstep.subway.station.dto.StationResponse;
-
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+
+import nextstep.subway.line.domain.Line;
+import nextstep.subway.station.dto.StationResponse;
 
 public class LineResponse {
     private Long id;
     private String name;
     private String color;
     private List<StationResponse> stations;
+
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
     private LocalDateTime createdDate;
+
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
     private LocalDateTime modifiedDate;
 
-    public LineResponse() {
-    }
+    public LineResponse() {}
 
-    public LineResponse(Long id, String name, String color, List<StationResponse> stations, LocalDateTime createdDate, LocalDateTime modifiedDate) {
+    public LineResponse(Long id, String name, String color, List<StationResponse> stations, LocalDateTime createdDate,
+            LocalDateTime modifiedDate) {
         this.id = id;
         this.name = name;
         this.color = color;
@@ -29,10 +40,12 @@ public class LineResponse {
     }
 
     public static LineResponse of(Line line) {
-        if(isEmpty(line)) {
-            return new LineResponse(line.getId(), line.getName(), line.getColor(), new ArrayList(), line.getCreatedDate(), line.getModifiedDate());
+        if (isEmpty(line)) {
+            return new LineResponse(line.getId(), line.getName(), line.getColor(), new ArrayList(),
+                line.getCreatedDate(), line.getModifiedDate());
         }
-        return new LineResponse(line.getId(), line.getName(), line.getColor(), assembleStations(line), line.getCreatedDate(), line.getModifiedDate());
+        return new LineResponse(line.getId(), line.getName(), line.getColor(), assembleStations(line),
+            line.getCreatedDate(), line.getModifiedDate());
     }
 
     private static boolean isEmpty(Line line) {
--- src/main/java/nextstep/subway/station/dto/StationResponse.java
+++ src/main/java/nextstep/subway/station/dto/StationResponse.java
@@ -1,21 +1,32 @@
 package nextstep.subway.station.dto;
 
-import nextstep.subway.station.domain.Station;
-
 import java.time.LocalDateTime;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+
+import nextstep.subway.station.domain.Station;
 
 public class StationResponse {
     private Long id;
     private String name;
+
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
     private LocalDateTime createdDate;
+
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
     private LocalDateTime modifiedDate;
 
     public static StationResponse of(Station station) {
-        return new StationResponse(station.getId(), station.getName(), station.getCreatedDate(), station.getModifiedDate());
+        return new StationResponse(station.getId(), station.getName(), station.getCreatedDate(),
+            station.getModifiedDate());
     }
 
-    public StationResponse() {
-    }
+    public StationResponse() {}
 
     public StationResponse(Long id, String name, LocalDateTime createdDate, LocalDateTime modifiedDate) {
         this.id = id;
