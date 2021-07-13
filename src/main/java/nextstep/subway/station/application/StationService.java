dadac7cc2530139d60395c0a28a8d32c682b0e03
Jung Insup <jisjis1218@gmail.com>
2021-07-13 18:27:30 +0900

    refactor(StationService): Cache 관련 annotation 추가

--- src/main/java/nextstep/subway/station/application/StationService.java
+++ src/main/java/nextstep/subway/station/application/StationService.java
@@ -4,6 +4,9 @@ import nextstep.subway.station.domain.Station;
 import nextstep.subway.station.domain.StationRepository;
 import nextstep.subway.station.dto.StationRequest;
 import nextstep.subway.station.dto.StationResponse;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,18 +16,22 @@ import java.util.stream.Collectors;
 @Service
 @Transactional
 public class StationService {
+    public static final String STATIONS_KEY = "stationskey";
+
     private StationRepository stationRepository;
 
     public StationService(StationRepository stationRepository) {
         this.stationRepository = stationRepository;
     }
 
+    @CachePut(value = "stations")
     public StationResponse saveStation(StationRequest stationRequest) {
         Station persistStation = stationRepository.save(stationRequest.toStation());
         return StationResponse.of(persistStation);
     }
 
     @Transactional(readOnly = true)
+    @Cacheable(value = "stations")
     public List<StationResponse> findAllStations() {
         List<Station> stations = stationRepository.findAll();
 
@@ -33,10 +40,13 @@ public class StationService {
                 .collect(Collectors.toList());
     }
 
+    @CacheEvict(value = "station", key = "#id")
+    @CachePut(value = "stations")
     public void deleteStationById(Long id) {
         stationRepository.deleteById(id);
     }
 
+    @Cacheable(value = "station", key = "#id")
     public Station findStationById(Long id) {
         return stationRepository.findById(id).orElseThrow(RuntimeException::new);
     }
