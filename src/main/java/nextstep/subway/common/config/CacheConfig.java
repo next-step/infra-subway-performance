7b4b238fc5265481c5166a8ff46a2b0d9c2e6c15
kyle <cupeanimus@naver.com>
2021-07-15 04:05:11 +0900

    refactor : remove AsyncThreadConfig.java

--- src/main/java/nextstep/subway/common/config/CacheConfig.java
+++ src/main/java/nextstep/subway/common/config/CacheConfig.java
@@ -13,7 +13,6 @@ import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSeriali
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
-@EnableCaching
 @Configuration
 public class CacheConfig extends CachingConfigurerSupport {
 
