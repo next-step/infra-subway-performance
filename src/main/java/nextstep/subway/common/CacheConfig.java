aec33b6722e937fabcec5bfbd6e591f7ff1a4b23
Yoon Jisoo <34590027+yjs2952@users.noreply.github.com>
2021-07-14 11:25:35 +0900

    Step1 (#42)
    * docs: 프로필별 설정 추가
    
    * test: 인수테스트 프로필 테스트로 설정
    
    * feat: 레디스 의존성 및 설정파일 추가
    
    * refactor: 레디스 설정 파일 디렉토리 변경
    
    * feat: 캐시 처리
    
    * docs: local db 계정 수정
    
    * fix: 캐시 적용 수정
    
    * fix: 날짜 deserialize 설정 추가
    
    * fix: 테스트 프로필에서 캐시 사용 안하도록 수정
    
    * feat: 비동기 설정 추가
    
    * docs: 운영 프로필 설정 변경
    
    * feat: mysql 의존성 추가
    
    * docs: 성능 개선 결과 및 방법 추가

---
+++ src/main/java/nextstep/subway/common/CacheConfig.java
@@ -0,0 +1,45 @@
+package nextstep.subway.common;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+
+@EnableCaching
+@Configuration
+@Profile("!test")
+public class CacheConfig extends CachingConfigurerSupport {
+
+    @Autowired
+    private RedisConnectionFactory connectionFactory;
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        return Jackson2ObjectMapperBuilder.json().featuresToDisable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS).modules(new JavaTimeModule()).build();
+    }
+
+    @Bean
+    public CacheManager redisCacheManager() {
+        RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
+                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()))
+        ;
+
+        RedisCacheManager redisCacheManager = RedisCacheManager.RedisCacheManagerBuilder.
+                fromConnectionFactory(connectionFactory).cacheDefaults(redisCacheConfiguration).build();
+        return redisCacheManager;
+    }
+}
--- src/main/java/nextstep/subway/station/application/StationService.java
+++ src/main/java/nextstep/subway/station/application/StationService.java
@@ -4,6 +4,9 @@ import nextstep.subway.station.domain.Station;
 import nextstep.subway.station.domain.StationRepository;
 import nextstep.subway.station.dto.StationRequest;
 import nextstep.subway.station.dto.StationResponse;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -33,14 +36,18 @@ public class StationService {
                 .collect(Collectors.toList());
     }
 
+    @Async
+    @CacheEvict(value="station", allEntries = true)
     public void deleteStationById(Long id) {
         stationRepository.deleteById(id);
     }
 
     public Station findStationById(Long id) {
-        return stationRepository.findById(id).orElseThrow(RuntimeException::new);
+        return findById(id);
     }
 
+    @Cacheable(value="station", key = "#id")
+    @Transactional(readOnly = true)
     public Station findById(Long id) {
         return stationRepository.findById(id).orElseThrow(RuntimeException::new);
     }
