60ce889c2239b8f31797c153a6d994410f292694
Insup Jung <31408300+jis1218@users.noreply.github.com>
2021-07-16 00:31:42 +0900

    refactor(DatabaseConfig): dev profile일 때만 적용되도록 어노테이션 추가

--- src/main/java/nextstep/subway/common/DatabaseConfig.java
+++ src/main/java/nextstep/subway/common/DatabaseConfig.java
@@ -9,7 +9,7 @@ import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.context.annotation.Profile;
 import org.springframework.jdbc.datasource.LazyConnectionDataSourceProxy;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
@@ -21,6 +21,7 @@ import static nextstep.subway.common.ReplicationRoutingDataSource.*;
 @Configuration
 @EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
 @EnableTransactionManagement
+@Profile("dev")
 public class DatabaseConfig {
     @Bean
     @ConfigurationProperties(prefix = "spring.datasource.hikari.master")
