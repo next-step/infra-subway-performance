e2e93d7087af8d5a5850f8bfa8d66710e1445de1
kyle <cupeanimus@naver.com>
2021-07-18 01:57:53 +0900

    fix : run setting
    실행 안되는 오류 수정 mysql connector 추가

--- src/main/java/nextstep/subway/common/config/DataBaseConfig.java
+++ src/main/java/nextstep/subway/common/config/DataBaseConfig.java
@@ -10,6 +10,7 @@ import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.LazyConnectionDataSourceProxy;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
@@ -22,6 +23,7 @@ import static nextstep.subway.common.ReplicationRoutingDataSource.DATASOURCE_KEY
 @Configuration
 @EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
 @EnableTransactionManagement
+@EnableJpaRepositories(basePackages = {"nextstep.subway"})
 class DataBaseConfig {
 
     @Bean
