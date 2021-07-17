e98667763a1da289b7ee6fe15636d6185e99d64b
fDevJc <fDevJc@gmail.com>
2021-07-17 14:30:39 +0900

    refactor: 특정인덱스 탐색 쿼리 추가

--- src/main/java/nextstep/subway/favorite/domain/FavoriteRepository.java
+++ src/main/java/nextstep/subway/favorite/domain/FavoriteRepository.java
@@ -4,10 +4,9 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import org.springframework.data.jpa.repository.Query;
 
 public interface FavoriteRepository extends JpaRepository<Favorite, Long> {
-
-    List<Favorite> findByMemberId(Long memberId);
-
+    @Query("select * from favorite f where f.id >= ?1")
     List<Favorite> findByMemberId(Long id, Pageable pageable);
 }
