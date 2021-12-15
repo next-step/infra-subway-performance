29f2e5fb09fcae5d5ecde7ec02293d1651ae8b93
peter-alethio <peter@alethio.io>
2021-12-15 23:10:54 +0900

    chore: 비동기 스레드 풀 설정

---
+++ src/main/java/nextstep/subway/config/AsyncThreadConfig.java
@@ -0,0 +1,23 @@
+package nextstep.subway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@Configuration
+@EnableAsync
+public class AsyncThreadConfig {
+
+    @Bean
+    public Executor asyncThreadTaskExecutor() {
+        ThreadPoolTaskExecutor exexcutor = new ThreadPoolTaskExecutor();
+        exexcutor.setCorePoolSize(2);
+        exexcutor.setMaxPoolSize(4);
+        exexcutor.setQueueCapacity(100);
+        exexcutor.setThreadNamePrefix("subway-async-");
+        return exexcutor;
+    }
+}
