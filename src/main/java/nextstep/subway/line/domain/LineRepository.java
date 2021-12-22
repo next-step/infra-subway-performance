3fa5d614ebe2eaead0076f2b3f68b432d0f4856b
cus4703 <cus4703@gmail.com>
2021-12-22 22:00:31 +0900

    fix: 페이징 수정

--- src/main/java/nextstep/subway/line/domain/LineRepository.java
+++ src/main/java/nextstep/subway/line/domain/LineRepository.java
@@ -1,11 +1,9 @@
 package nextstep.subway.line.domain;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-import java.util.List;
-
 public interface LineRepository extends JpaRepository<Line, Long> {
-    @Query("select l from Line l where l.id >= ?1" )
-    List<Line> findAll();
 }
--- src/main/java/nextstep/subway/station/domain/StationRepository.java
+++ src/main/java/nextstep/subway/station/domain/StationRepository.java
@@ -1,12 +1,11 @@
 package nextstep.subway.station.domain;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-import java.util.List;
-
 public interface StationRepository extends JpaRepository<Station, Long> {
     @Override
-    @Query("select s from Station s where s.id >= ?1")
-    List<Station> findAll();
+    Page<Station> findAll(Pageable pageable);
 }
