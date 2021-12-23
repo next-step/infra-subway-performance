d6fcefb49ee11ae25c7aa4892e6f916af76e9bed
김도영 <95doyoung@gmail.com>
2021-12-23 23:24:34 +0900

    fix (config) : cache config profile 수정

--- src/main/java/nextstep/subway/common/CacheConfig.java
+++ src/main/java/nextstep/subway/common/CacheConfig.java
@@ -13,7 +13,7 @@ import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSeriali
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
-@Profile("local,prod")
+@Profile("local | prod")
 @EnableCaching
 @Configuration
 public class CacheConfig extends CachingConfigurerSupport {
