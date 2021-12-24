0b1f0306e70b4844c696133baf412c126308a153
lovethefeel <lovethefeel@gmail.com>
2021-12-24 23:51:22 +0900

    refactor: 멤버 레파지토리 페이지 목록 메소드 추가

--- src/main/java/nextstep/subway/member/domain/MemberRepository.java
+++ src/main/java/nextstep/subway/member/domain/MemberRepository.java
@@ -1,9 +1,13 @@
 package nextstep.subway.member.domain;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findByEmail(String email);
+
+    Page<Member> findAll(Pageable pg);
 }
