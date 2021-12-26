e8bef843a27fdb2261d15d2b36f68c6725a2eec9
jerry92k <jens92.k@gmail.com>
2021-12-26 10:57:51 +0900

    feat : 캐싱 추가 - 경로 찾기 캐싱   - ```노선 추가, 노선 삭제, 노선에 구간 추가, 노선에 구간 삭제```의 경우, 캐싱 삭제 - 노선 캐싱   - 특정 노선에 대해 ```입력, 수정, 삭제``` 이벤트가 발생할 경우 ```전체 조회``` 캐싱 삭제 - 지하철역 캐싱   - 특정 지하철역에 대해 ```입력, 수정, 삭제``` 이벤트가 발생할 경우 ```전체 조회``` 캐싱 삭제 - 트랜젝션 어노테이션   - 기본을 클래스 레벨에서 readOnly로 하고 데이터 변경에 관여하는 트랜젝션은 별도 명시

--- src/main/java/nextstep/subway/station/application/StationService.java
+++ src/main/java/nextstep/subway/station/application/StationService.java
@@ -4,6 +4,9 @@ import nextstep.subway.station.domain.Station;
 import nextstep.subway.station.domain.StationRepository;
 import nextstep.subway.station.dto.StationRequest;
 import nextstep.subway.station.dto.StationResponse;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.Caching;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -11,28 +14,39 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
-@Transactional
+@Transactional(readOnly = true)
 public class StationService {
+
+    public static final String ALL_STATIONS_CACHE_KEY = "allStationKey";
+
     private StationRepository stationRepository;
 
     public StationService(StationRepository stationRepository) {
         this.stationRepository = stationRepository;
     }
 
+    @Transactional
+    @CacheEvict(value = "station", key = "#root.target.ALL_STATIONS_CACHE_KEY")
     public StationResponse saveStation(StationRequest stationRequest) {
         Station persistStation = stationRepository.save(stationRequest.toStation());
         return StationResponse.of(persistStation);
     }
 
-    @Transactional(readOnly = true)
-    public List<StationResponse> findAllStations() {
-        List<Station> stations = stationRepository.findAll();
+    @Cacheable(value = "station", key = "#root.target.ALL_STATIONS_CACHE_KEY")
+    public List<StationResponse> findAllStationResponses() {
+        List<Station> stations = findAllStations();
 
         return stations.stream()
-                .map(StationResponse::of)
-                .collect(Collectors.toList());
+            .map(StationResponse::of)
+            .collect(Collectors.toList());
     }
 
+    public List<Station> findAllStations() {
+        return stationRepository.findAll();
+    }
+
+    @Transactional
+    @CacheEvict(value = "station", key = "#root.target.ALL_STATIONS_CACHE_KEY")
     public void deleteStationById(Long id) {
         stationRepository.deleteById(id);
     }
