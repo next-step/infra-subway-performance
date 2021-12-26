ae534e1ab46f3ea2ba7adc87029edbae6dfb8167
minseoklim <mslim8803@naver.com>
2021-12-26 19:24:39 +0900

    test: k6테스트 VUser 수 변경

--- k6/load.js
+++ k6/load.js
@@ -2,7 +2,7 @@ import http from 'k6/http';
 import {check, sleep} from 'k6';
 
 export let options = {
-    vus: 20,
+    vus: 5,
     duration: '10s',
 
     thresholds: {
--- k6/stress.js
+++ k6/stress.js
@@ -2,7 +2,7 @@ import http from 'k6/http';
 import {check, sleep} from 'k6';
 
 export let options = {
-    vus: 50,
+    vus: 20,
     duration: '10s',
 
     thresholds: {
