dc8a57be461972a4773ac6b807ba59f001c186a8
wooobo <irooori89@gmail.com>
2021-12-26 13:08:08 +0900

    feat : `FavoriteRepository` pageable 추가

--- src/main/java/nextstep/subway/favorite/domain/FavoriteRepository.java
+++ src/main/java/nextstep/subway/favorite/domain/FavoriteRepository.java
@@ -1,8 +1,10 @@
 package nextstep.subway.favorite.domain;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import org.springframework.data.jpa.repository.Query;
 
 public interface FavoriteRepository extends JpaRepository<Favorite, Long> {
 
