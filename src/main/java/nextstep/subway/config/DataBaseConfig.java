ee5fd6c66e72d5454047c2d0616216be02727164
wooobo <irooori89@gmail.com>
2021-12-26 14:07:24 +0900

    feat : `Station` 페이징 처리 추가

--- src/main/java/nextstep/subway/config/DataBaseConfig.java
+++ src/main/java/nextstep/subway/config/DataBaseConfig.java
@@ -12,6 +12,7 @@ import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.LazyConnectionDataSourceProxy;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -23,6 +24,7 @@ import java.util.HashMap;
 @EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
 @EnableTransactionManagement
 @EnableJpaRepositories(basePackages = {"nextstep.subway"})
+@Profile("prod | local")
 class DatabaseConfig {
 
     @Bean
