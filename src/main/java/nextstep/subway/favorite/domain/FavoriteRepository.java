95459c912052027d01b4bf181963b6e03760b2a8
wooobo <irooori89@gmail.com>
2021-12-26 13:08:04 +0900

    feat : `FavoriteRepository` pageable 추가

--- src/main/java/nextstep/subway/favorite/domain/FavoriteRepository.java
+++ src/main/java/nextstep/subway/favorite/domain/FavoriteRepository.java
@@ -5,5 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface FavoriteRepository extends JpaRepository<Favorite, Long> {
-    List<Favorite> findByMemberId(Long memberId);
+
+    @Query("SELECT f FROM Favorite f WHERE f.memberId >= ?1")
+    List<Favorite> findByMemberId(Long memberId, Pageable pageable);
 }
