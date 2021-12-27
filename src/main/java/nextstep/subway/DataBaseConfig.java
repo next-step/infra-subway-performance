e05d01156e21d6151b7b90121d985262479f8f5a
JiHun Hong <hongji3354@gmail.com>
2021-12-27 14:33:53 +0900

    fix : profile이 test가 아닐 시 MYSQL Replication 설정이 Bean으로 등록되도록 변경

--- src/main/java/nextstep/subway/DataBaseConfig.java
+++ src/main/java/nextstep/subway/DataBaseConfig.java
@@ -9,6 +9,7 @@ import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 import org.springframework.jdbc.datasource.LazyConnectionDataSourceProxy;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
@@ -18,6 +19,7 @@ import java.util.HashMap;
 @Configuration
 @EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
 @EnableTransactionManagement
+@Profile("!test")
 public class DataBaseConfig {
 
     @Bean
