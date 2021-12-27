795e8bfd91c9cc91e0d0e456717c8f3186499778
minseokLim <36442984+minseokLim@users.noreply.github.com>
2021-12-27 14:33:54 +0900

    1단계 - 화면 응답 개선하기 (#126)
    * test: 부하 테스트 스크립트 추가
    
    * feat: Line, Station, Member에 Redis 캐싱 처리
    
    * chore: DB 초기 데이터 세팅
    
    * test: k6 스크립트에서 조회 로직만 남기고 삭제
    
    * feat: 레디스 설정 disable
    
    * test: k6테스트 VUser 수 변경
    
    * docs(README.md): 답변 작성

---
+++ k6/stress.js
@@ -0,0 +1,67 @@
+import http from 'k6/http';
+import {check, sleep} from 'k6';
+
+export let options = {
+    vus: 20,
+    duration: '10s',
+
+    thresholds: {
+        http_req_duration: ['p(99)<1500'], // 99% of requests must complete below 1.5s
+    },
+};
+
+const BASE_URL = 'https://minseoklim.p-e.kr';
+const USERNAME = 'test@test.com';
+const PASSWORD = 'password';
+const AGE = 20;
+
+export default function () {
+    const before = new Date().getTime();
+    const T = 6 * 1.5;
+
+    let jsonHeaders = {
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    };
+
+    let htmlHeaders = {
+        headers: {
+            'Content-Type': 'text/html',
+        },
+    };
+
+    // 지하철역 목록 조회 화면
+    let stationsPage = http.get(`${BASE_URL}/stations`, htmlHeaders);
+    check(stationsPage, {'retrieved stationsPage': (obj) => obj != null});
+
+    // 지하철역 목록 조회
+    let stations = http.get(`${BASE_URL}/stations`, jsonHeaders).json();
+    check(stations, {'retrieved stations': (obj) => obj != null});
+
+    // 경로 검색 화면
+    let pathPage = http.get(`${BASE_URL}/path`, htmlHeaders);
+    check(pathPage, {'retrieved pathPage': (obj) => obj != null});
+
+    // 경로 검색
+    let paths = http.get(`${BASE_URL}/paths/?source=1&target=2`, jsonHeaders).json();
+    check(paths, {'retrieved paths': (obj) => obj != null});
+
+    // 노선 조회 화면
+    let linesPage = http.get(`${BASE_URL}/lines`, htmlHeaders);
+    check(linesPage, {'retrieved linesPage': (obj) => obj != null});
+
+    // 노선 조회
+    let lines = http.get(`${BASE_URL}/lines`, jsonHeaders).json();
+    check(lines, {'retrieved lines': (obj) => obj != null});
+
+    const after = new Date().getTime();
+    const diff = (after - before) / 1000;
+    const remainder = T - diff;
+    check(remainder, {'reached request rate': remainder > 0});
+    if (remainder > 0) {
+        sleep(remainder);
+    } else {
+        console.warn(`Timer exhausted! The execution time of the test took longer than ${T} seconds`);
+    }
+};
---
+++ src/main/java/nextstep/subway/common/CacheConfig.java
@@ -0,0 +1,35 @@
+package nextstep.subway.common;
+
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+// @EnableCaching
+// @Configuration
+public class CacheConfig extends CachingConfigurerSupport {
+    private final RedisConnectionFactory connectionFactory;
+
+    public CacheConfig(final RedisConnectionFactory connectionFactory) {
+        this.connectionFactory = connectionFactory;
+    }
+
+    // @Bean
+    public CacheManager redisCacheManager() {
+        final RedisSerializationContext.SerializationPair<String> stringRedisSerializer =
+            RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer());
+        final RedisSerializationContext.SerializationPair<Object> jsonRedisSerializer =
+            RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer());
+
+        final RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
+            .serializeKeysWith(stringRedisSerializer)
+            .serializeValuesWith(jsonRedisSerializer);
+
+        return RedisCacheManager.RedisCacheManagerBuilder.
+            fromConnectionFactory(connectionFactory).cacheDefaults(redisCacheConfiguration).build();
+    }
+}
