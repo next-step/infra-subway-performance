e1611ad4e3afab0043479e82bbac6100efae5de3
김도영 <95doyoung@gmail.com>
2021-12-27 14:27:43 +0900

    refactor : slave DB 를 위한 read only 옵션 추가

--- src/main/java/nextstep/subway/line/domain/LineRepository.java
+++ src/main/java/nextstep/subway/line/domain/LineRepository.java
@@ -4,9 +4,10 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface LineRepository extends JpaRepository<Line, Long> {
 
-    @Query("SELECT line FROM Line line WHERE line.id > ?1")
-    Slice<Line> findAll(Long offset, Pageable pageable);
+    @Query("SELECT line FROM Line line WHERE line.id > :offset")
+    Slice<Line> findAll(@Param("offset") Long offset, Pageable pageable);
 }
