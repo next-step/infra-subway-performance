bba7ca28660780d431f0d4cc064d1888733b3991
Chaeyun Lee <chaeyunn@iochord.com>
2021-12-28 21:08:15 +0900

    feat: 페이지네이션

--- src/main/java/nextstep/subway/station/domain/StationRepository.java
+++ src/main/java/nextstep/subway/station/domain/StationRepository.java
@@ -1,10 +1,14 @@
 package nextstep.subway.station.domain;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface StationRepository extends JpaRepository<Station, Long> {
-    @Override
-    List<Station> findAll();
+
+    @Query(value = "SELECT s FROM Station s WHERE s.id > :offset",
+        countQuery = "SELECT count(s) FROM Station s")
+    Page<Station> findAll(@Param("offset") Long offset, Pageable pageable);
 }
