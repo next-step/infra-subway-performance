da8953d6fd0bcd874f1bd18885e38c3498293f94
All-ForOne <ddabizz@gmail.com>
2021-12-29 22:26:34 +0900

    feat: 페이징 적용

--- src/main/java/nextstep/subway/station/domain/StationRepository.java
+++ src/main/java/nextstep/subway/station/domain/StationRepository.java
@@ -1,10 +1,13 @@
 package nextstep.subway.station.domain;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface StationRepository extends JpaRepository<Station, Long> {
-    @Override
-    List<Station> findAll();
+    @Query("SELECT station FROM Station station WHERE station.id > :offset")
+    List<Station> findAll(@Param("offset") Long offset, Pageable pageable);
 }
