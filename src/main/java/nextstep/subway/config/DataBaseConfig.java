28c66cad0c3dd95507d87e84ebc178d87e0c9c2e
songseongbin <ppsosqq@gmail.com>
2021-12-30 18:18:27 +0900

    config: slave master datasource는 운영에서만 적용

--- src/main/java/nextstep/subway/config/DataBaseConfig.java
+++ src/main/java/nextstep/subway/config/DataBaseConfig.java
@@ -14,6 +14,7 @@ import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.LazyConnectionDataSourceProxy;
@@ -21,6 +22,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import com.zaxxer.hikari.HikariDataSource;
 
+@Profile("prod")
 @EnableJpaAuditing
 @Configuration
 @EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
