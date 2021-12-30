87427fee048da924bd5bb90fd506df5747dfa29c
minho <lights8615@gmail.com>
2021-12-31 05:37:24 +0900

    feat(step2): 환경 분리

--- src/main/java/nextstep/subway/DataBaseConfig.java
+++ src/main/java/nextstep/subway/DataBaseConfig.java
@@ -14,6 +14,7 @@ import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.LazyConnectionDataSourceProxy;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -24,6 +25,7 @@ import com.zaxxer.hikari.HikariDataSource;
 @EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
 @EnableTransactionManagement
 @EnableJpaRepositories(basePackages = {"nextstep.subway"})
+@Profile("!test")
 class DataBaseConfig {
 
     @Bean
