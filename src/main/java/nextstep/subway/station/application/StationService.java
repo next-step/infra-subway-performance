9075122c8e7d6c9d00263647d19112227a41c215
minseoklim <mslim8803@naver.com>
2021-12-30 19:06:28 +0900

    feat(station): 역 목록 조회 API 페이징 처리

--- src/main/java/nextstep/subway/station/application/StationService.java
+++ src/main/java/nextstep/subway/station/application/StationService.java
@@ -1,21 +1,26 @@
 package nextstep.subway.station.application;
 
+import java.util.Arrays;
+
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import nextstep.subway.station.domain.Station;
 import nextstep.subway.station.domain.StationRepository;
 import nextstep.subway.station.dto.StationRequest;
 import nextstep.subway.station.dto.StationResponse;
 
-import org.springframework.cache.annotation.CacheEvict;
-import org.springframework.cache.annotation.Cacheable;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
-import java.util.stream.Collectors;
-
 @Service
 @Transactional
 public class StationService {
+    private static final String ID = "id";
+
     private StationRepository stationRepository;
 
     public StationService(StationRepository stationRepository) {
@@ -28,12 +33,32 @@ public class StationService {
     }
 
     @Transactional(readOnly = true)
-    public List<StationResponse> findAllStations() {
-        List<Station> stations = stationRepository.findAll();
+    public Page<StationResponse> findAllStations(final Pageable pageable) {
+        if (sortedById(pageable.getSort())) {
+            final Specification<Station> specification = computeSpecification(pageable);
+            return stationRepository.findAll(specification, pageable)
+                .map(StationResponse::of);
+        }
 
-        return stations.stream()
-                .map(StationResponse::of)
-                .collect(Collectors.toList());
+        return stationRepository.findAll(pageable)
+            .map(StationResponse::of);
+    }
+
+    private boolean sortedById(final Sort sort) {
+        return Arrays.asList(Sort.by(ID).ascending(), Sort.by(ID).descending(), Sort.unsorted()).contains(sort);
+    }
+
+    private Specification<Station> computeSpecification(final Pageable pageable) {
+        final Sort.Order order = pageable.getSort().getOrderFor(ID);
+
+        if (order != null && order.getDirection() == Sort.Direction.DESC) {
+            final Long maxId = stationRepository.getMaxId();
+            final long boundaryId = maxId - (long)pageable.getPageNumber() * pageable.getPageSize();
+            return (root, query, criteriaBuilder) -> criteriaBuilder.lessThanOrEqualTo(root.get(ID), boundaryId);
+        }
+
+        final long boundaryId = (long)pageable.getPageNumber() * pageable.getPageSize();
+        return (root, query, criteriaBuilder) -> criteriaBuilder.greaterThan(root.get(ID), boundaryId);
     }
 
     @CacheEvict(value = "station", key = "#id")
--- src/main/java/nextstep/subway/station/domain/StationRepository.java
+++ src/main/java/nextstep/subway/station/domain/StationRepository.java
@@ -1,10 +1,19 @@
 package nextstep.subway.station.domain;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface StationRepository extends JpaRepository<Station, Long> {
     @Override
     List<Station> findAll();
+
+    Page<Station> findAll(Specification<Station> specification, Pageable pageable);
+
+    @Query("select coalesce(max(s.id), 0) from Station s")
+    Long getMaxId();
 }
