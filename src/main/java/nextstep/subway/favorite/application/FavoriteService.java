680b16921ed19281ccf444fce941bf9d54f45561
SteadyJin <64358371+changjinpark@users.noreply.github.com>
2022-01-01 20:47:03 +0900

    Step2 - 조회 성능 개선하기 (#122)
    * feat: findByMemberId 페이징 쿼리 적용
    
    * chore: 운영 DB connection을 위한 설정 파일 추가
    
    - 운영 DB connection을 위한 url 추가
    - gzip 압축 설정
    - 캐시 설정
    
    * chore: build.gradle의 mysql-connector-java 의존성 추가
    
    * feat: 데이터베이스 이중화
    
    * docs: 2단계 조회 성능 개선하기 정리
    
    * refactor: Page 인터페이스와 구현체인 PageImpl 추가
    
    * docs: programmer 인덱스 설정 확인

--- src/main/java/nextstep/subway/favorite/application/FavoriteService.java
+++ src/main/java/nextstep/subway/favorite/application/FavoriteService.java
@@ -9,7 +9,11 @@ import nextstep.subway.favorite.dto.FavoriteResponse;
 import nextstep.subway.station.domain.Station;
 import nextstep.subway.station.domain.StationRepository;
 import nextstep.subway.station.dto.StationResponse;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.HashSet;
 import java.util.List;
@@ -33,16 +37,22 @@ public class FavoriteService {
         favoriteRepository.save(favorite);
     }
 
-    public List<FavoriteResponse> findFavorites(LoginMember loginMember) {
-        List<Favorite> favorites = favoriteRepository.findByMemberId(loginMember.getId());
+    @Transactional(readOnly = true)
+    public Page<FavoriteResponse> findFavorites(LoginMember loginMember, Pageable pageable) {
+        Page<Favorite> favorites = favoriteRepository.findByMemberId(loginMember.getId(), pageable);
         Map<Long, Station> stations = extractStations(favorites);
 
-        return favorites.stream()
-            .map(it -> FavoriteResponse.of(
-                it,
-                StationResponse.of(stations.get(it.getSourceStationId())),
-                StationResponse.of(stations.get(it.getTargetStationId()))))
-            .collect(Collectors.toList());
+        List<FavoriteResponse> favoriteResponses = convertFavoriteResponses(favorites, stations);
+        return new PageImpl<>(favoriteResponses, pageable, favorites.getTotalElements());
+    }
+
+    private List<FavoriteResponse> convertFavoriteResponses(Page<Favorite> favorites, Map<Long, Station> stations) {
+        return favorites.getContent().stream()
+                .map(it -> FavoriteResponse.of(
+                        it,
+                        StationResponse.of(stations.get(it.getSourceStationId())),
+                        StationResponse.of(stations.get(it.getTargetStationId()))))
+                .collect(Collectors.toList());
     }
 
     public void deleteFavorite(LoginMember loginMember, Long id) {
@@ -53,13 +63,13 @@ public class FavoriteService {
         favoriteRepository.deleteById(id);
     }
 
-    private Map<Long, Station> extractStations(List<Favorite> favorites) {
+    private Map<Long, Station> extractStations(Page<Favorite> favorites) {
         Set<Long> stationIds = extractStationIds(favorites);
         return stationRepository.findAllById(stationIds).stream()
             .collect(Collectors.toMap(Station::getId, Function.identity()));
     }
 
-    private Set<Long> extractStationIds(List<Favorite> favorites) {
+    private Set<Long> extractStationIds(Page<Favorite> favorites) {
         Set<Long> stationIds = new HashSet<>();
         for (Favorite favorite : favorites) {
             stationIds.add(favorite.getSourceStationId());
--- src/main/java/nextstep/subway/favorite/domain/FavoriteRepository.java
+++ src/main/java/nextstep/subway/favorite/domain/FavoriteRepository.java
@@ -1,9 +1,11 @@
 package nextstep.subway.favorite.domain;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
+import org.springframework.data.jpa.repository.Query;
 
 public interface FavoriteRepository extends JpaRepository<Favorite, Long> {
-    List<Favorite> findByMemberId(Long memberId);
+    @Query("select favorite from Favorite favorite where favorite.id >= ?1")
+    Page<Favorite> findByMemberId(Long memberId, Pageable pageable);
 }
