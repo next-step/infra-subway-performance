8c527a56abd340e6f49da7dba90162a35d78fe37
tmdgusya <tmdgusya@naver.com>
2022-05-06 23:15:04 +0900

    [미션 1단계] Cache Config 및 캐싱 처리

--- src/main/java/nextstep/subway/map/ui/MapController.java
+++ src/main/java/nextstep/subway/map/ui/MapController.java
@@ -1,7 +1,13 @@
 package nextstep.subway.map.ui;
 
+import static net.logstash.logback.argument.StructuredArguments.kv;
+
 import nextstep.subway.map.application.MapService;
 import nextstep.subway.map.dto.PathResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,14 +15,17 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MapController {
+    private static final Logger json = LoggerFactory.getLogger("json");
     private MapService mapService;
 
     public MapController(MapService mapService) {
         this.mapService = mapService;
     }
 
+    @Cacheable(value = "findPath", key = "#source-#target")
     @GetMapping("/paths")
     public ResponseEntity<PathResponse> findPath(@RequestParam Long source, @RequestParam Long target) {
+        json.info("[경로검색] {}, {}", kv("출발지", source), kv("목적지", target));
         return ResponseEntity.ok(mapService.findPath(source, target));
     }
 }
