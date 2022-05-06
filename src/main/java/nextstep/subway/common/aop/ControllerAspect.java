0577e25002e85ea82228aa88b2a11d9e5d0e03a4
saerang <lsg10119@gamail.com>
2022-05-06 23:10:40 +0900

    feat: 기존 설정 추가

---
+++ src/main/java/nextstep/subway/common/aop/ControllerAspect.java
@@ -0,0 +1,45 @@
+package nextstep.subway.common.aop;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+@Aspect
+@Component
+public class ControllerAspect {
+    private static final Logger log = LoggerFactory.getLogger("file");
+
+    @Pointcut("execution(* nextstep.subway..*.ui.*Controller.*(..) )")
+    public void controllerAdvice() {
+    }
+
+    @AfterThrowing(pointcut = "controllerAdvice()", throwing = "e")
+    public void logAfterThrowing(JoinPoint joinPoint, Throwable e) {
+        log.info("Exception in {}.{}() with cause = \'{}\' and exception = \'{}\'", joinPoint.getSignature().getDeclaringTypeName(),
+                  joinPoint.getSignature().getName(), e.getCause() != null ? e.getCause() : "NULL", e.getMessage(), e);
+    }
+
+    @Before("controllerAdvice()")
+    public void requestLogging(JoinPoint joinPoint) {
+        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
+        log.info("[" + joinPoint.getSignature().toShortString() + "] -------------------------- Controller START --------------------------");
+        log.info("[" + joinPoint.getSignature().toShortString() + "] URL : " + request.getMethod() + " " + request.getRequestURI() );
+        log.info("[" + joinPoint.getSignature().toShortString() + "] Controller Parameters: " + Arrays.stream(joinPoint.getArgs()).map(String::valueOf).collect(
+                Collectors.joining(",", "[", "]")));
+    }
+
+    @AfterReturning(pointcut = "controllerAdvice()", returning = "result")
+    public void requestLogging(JoinPoint joinPoint, Object result) {
+        log.info("[" + joinPoint.getSignature().toShortString() + "] Controller Returned: " + result);
+        log.info("[" + joinPoint.getSignature().toShortString() + "] -------------------------- Controller FINISH --------------------------");
+
+    }
+}
---
+++ src/main/java/nextstep/subway/common/aop/ServiceAspect.java
@@ -0,0 +1,48 @@
+package nextstep.subway.common.aop;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+@Aspect
+@Component
+public class ServiceAspect {
+    private static final Logger log = LoggerFactory.getLogger("file");
+
+    @Pointcut("execution(* nextstep.subway..*.application.*Service.*(..) )")
+    public void serviceAdvice() {
+    }
+
+    @AfterThrowing(pointcut = "serviceAdvice()", throwing = "e")
+    public void logAfterThrowing(JoinPoint joinPoint, Throwable e) {
+        log.info("Exception in {}.{}() with cause = \'{}\' and exception = \'{}\'", joinPoint.getSignature().getDeclaringTypeName(),
+                  joinPoint.getSignature().getName(), e.getCause() != null ? e.getCause() : "NULL", e.getMessage(), e);
+    }
+
+    @Before("serviceAdvice()")
+    public void requestLogging(JoinPoint joinPoint) {
+        log.info("[" + joinPoint.getSignature().toShortString() + "] -------------------------- Service START --------------------------");
+        log.info("[" + joinPoint.getSignature().toShortString() + "] Service Parameters: " + Arrays.toString(joinPoint.getArgs()));
+    }
+
+    @AfterReturning(pointcut = "serviceAdvice()", returning = "result")
+    public void requestLogging(JoinPoint joinPoint, Object result) {
+        log.info("[" + joinPoint.getSignature().toShortString() + "] -------------------------- Service FINISH --------------------------");
+    }
+
+    @AfterReturning(pointcut = "serviceAdvice()", returning = "result")
+    public void afterReturning(JoinPoint joinPoint, Object result) {
+        if (result instanceof Collection) {
+            result = ((Collection) result).stream().limit(100).collect(Collectors.toList());
+        }
+
+        log.info("[" + joinPoint.getSignature().toShortString() + "] Service Returned: " + result);
+        log.info("[" + joinPoint.getSignature().toShortString() + "] -------------------------- Service FINISH --------------------------");
+    }
+}
