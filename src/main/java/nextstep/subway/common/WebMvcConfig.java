51287f6a33275ed85bbbc70aa854be6f5e224193
shineoov <shineoov88@gmail.com>
2022-05-09 18:08:05 +0900

    HTTP 캐싱 적용

---
+++ src/main/java/nextstep/subway/common/WebMvcConfig.java
@@ -0,0 +1,22 @@
+package nextstep.subway.common;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+        registry.addResourceHandler("/js/**")
+                .addResourceLocations("classpath:/static/js/")
+                .setCacheControl(CacheControl.noCache().cachePrivate());
+
+        registry.addResourceHandler("/css/**")
+                .addResourceLocations("classpath:/static/css/")
+                .setCachePeriod(60 * 60 * 24 * 365);
+    }
+}
