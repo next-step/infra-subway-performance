3f91deb8758b3a6a9b5f8a7d8d5cc0287ff796f6
toughchb <ggang2000@gmail.com>
2022-06-28 13:01:40 +0900

    feat: 테스트를 위한 기본 설정 추가 및 스크립트 추가

---
+++ testscript/load.js
@@ -0,0 +1,91 @@
+import http from 'k6/http';
+import { check, group, sleep, fail } from 'k6';
+
+export let options = {
+    stages: [
+        { duration: '1m', target: 24 },
+        { duration: '3m', target: 24 },
+        { duration: '6m', target: 96 },
+        { duration: '6m', target: 96 },
+        { duration: '3m', target: 24 },
+        { duration: '1m', target: 24 }
+    ],
+    thresholds: {
+        http_req_duration: ['p(99)<1000'], // 99% of requests must complete below 1s
+    },
+};
+
+const BASE_URL = 'https://toughchb.kro.kr';
+const USERNAME = 'toughchb@gmail.com';
+const PASSWORD = 'toughchb';
+
+export default function ()  {
+    // 메인 페이지
+    mainPage();
+
+    // 로그인
+    let token = login();
+
+    // 나의 정보 수정
+    changeMember(token);
+
+    // 경로 검색
+    getPath(15, 27);
+
+    sleep(1);
+};
+
+function mainPage() {
+    let mainRes = http.get(`${BASE_URL}`);
+    check(mainRes, {
+        'mainPage successfully': (resp) => resp.status == 200
+    });
+}
+
+function login() {
+    var payload = JSON.stringify({
+        email: USERNAME,
+        password: PASSWORD,
+    });
+
+    var params = {
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    };
+
+    let loginRes = http.post(`${BASE_URL}/login/token`, payload, params);
+    check(loginRes, {
+        'logged in successfully': (resp) => resp.json('accessToken') !== '',
+    });
+
+    return loginRes.json('accessToken');
+}
+
+function changeMember(token) {
+
+    var payload = JSON.stringify({
+        email: USERNAME,
+        password: PASSWORD,
+        age: 32
+    });
+
+    var params = {
+        headers: {
+            Authorization: `Bearer ${token}`,
+            "Content-Type": "application/json"
+        }
+    };
+
+    let changeMemberRes = http.put(`${BASE_URL}/members/me`, payload, params);
+    check(changeMemberRes, {
+        'changeMember successfully': (response) => response.status === 200
+    });
+}
+
+function getPath(source, target) {
+    let pathRes = http.get(BASE_URL+'/path?source=' + source + '&target=' + target);
+    check(pathRes, {
+        'getPath successfully': (resp) => resp.status == 200
+    } );
+}
