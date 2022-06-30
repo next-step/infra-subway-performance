c8646b1b2f16d5b6f1fce640b74159cc9327bd3c
TakJong Oh <43510811+tj3828@users.noreply.github.com>
2022-07-01 01:14:37 +0900

    1단계 - 화면 응답 개선하기 (#241)
    * feat: applicaion properties 설정
    
    * docs: before k6 지표 추가
    
    * feat: redis setting & cache 설정 추가
    
    * feat: active test 설정 추가
    
    * docs: 개선 후 측정 결과
    
    * docs: 1단계 결과 작성
    
    * docs: 링크 수정

--- README.md
+++ README.md
@@ -44,9 +44,15 @@ npm run dev
 
 ### 1단계 - 화면 응답 개선하기
 1. 성능 개선 결과를 공유해주세요 (Smoke, Load, Stress 테스트 결과)
+   - [after](/docs/after)
+   - [before](/docs/before)
 
 2. 어떤 부분을 개선해보셨나요? 과정을 설명해주세요
 
+    - nginx cache, http2, gzip
+    - spring redis 적용
+
+    - pagespeed 성능지표 기준 67 -> 94 상승
 ---
 
 ### 2단계 - 스케일 아웃
---
+++ docs/script/smoke.js
@@ -0,0 +1,67 @@
+import http from 'k6/http';
+import { check, sleep, } from 'k6';
+
+export let options = {
+    vus: 2,
+    duration: '1m',
+    thresholds: {
+        http_req_duration: ['p(99)<1000'], // 99% of requests must complete below 1.5s
+    },
+};
+
+const BASE_URL = 'https://www.tj3828.p-e.kr';
+const USERNAME = 'email@email.com';
+const PASSWORD = 'password';
+
+export default function ()  {
+
+    mainPage();
+
+    let token = login();
+    let authHeaders = {
+        headers: {
+            Authorization: `Bearer ${token}`,
+        },
+    };
+
+    myInfo(authHeaders);
+    pathFinderPage();
+    pathFind();
+
+
+    sleep(1);
+};
+
+function mainPage() {
+    let main = http.get(BASE_URL);
+    check(main, {"메인 페이지 접근" : (resp) => resp.status === 200});
+}
+
+function login() {
+    var payload = JSON.stringify({
+        email: USERNAME,
+        password: PASSWORD,
+    });
+
+    var params = {headers: { 'Content-Type': 'application/json' }};
+
+    let loginRes = http.post(`${BASE_URL}/login/token`, payload, params);
+    check(loginRes, {"로그인 성공" : (resp) => resp.json('accessToken') !== ''});
+
+    return loginRes.json('accessToken');
+}
+
+function myInfo(authHeaders) {
+    let myInfo = http.get(`${BASE_URL}/members/me`, authHeaders).json();
+    check(myInfo, { '나의 정보 조회' : (obj) => obj.id !== 0 });
+}
+
+function pathFinderPage() {
+    let path = http.get(BASE_URL+'/path');
+    check(path, { '경로 조회 검색 페이지 접근': (resp) => resp.status === 200} );
+}
+
+function pathFind() {
+    let pathFind = http.get(BASE_URL+'/path?source=5&target=6');
+    check(pathFind, { '경로 조회 검색 성공': (resp) => resp.status === 200} );
+}
