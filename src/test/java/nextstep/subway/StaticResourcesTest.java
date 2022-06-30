c3931160eb07ffda2c592c9042671c8b278f9ca7
limwoobin <drogba02@naver.com>
2022-06-30 21:18:17 +0900

    test: js / css cache 설정이 올바른지 테스트 코드를 추가하라

---
+++ src/test/java/nextstep/subway/StaticResourcesTest.java
@@ -0,0 +1,79 @@
+package nextstep.subway;
+
+import static nextstep.subway.config.WebMvcConfig.PREFIX_STATIC_RESOURCES;
+
+import java.time.Duration;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.CacheControl;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.reactive.server.EntityExchangeResult;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@ActiveProfiles("test")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class StaticResourcesTest {
+    private static final Logger logger = LoggerFactory.getLogger(StaticResourcesTest.class);
+
+    @Autowired
+    private WebTestClient client;
+
+    @Test
+    void get_static_resources_js() {
+        String uri = PREFIX_STATIC_RESOURCES + "/static/js/main.js";
+        EntityExchangeResult<String> response = client
+            .get()
+            .uri(uri)
+            .exchange()
+            .expectStatus()
+            .isOk()
+            .expectHeader()
+            .cacheControl(CacheControl.noCache().cachePrivate())
+            .expectBody(String.class)
+            .returnResult();
+
+        logger.debug("body : {}", response.getResponseBody());
+
+        String etag = response.getResponseHeaders()
+            .getETag();
+
+        client
+            .get()
+            .uri(uri)
+            .header("If-None-Match", etag)
+            .exchange()
+            .expectStatus()
+            .isNotModified();
+    }
+
+    @Test
+    void get_static_resources_css() {
+        String uri = PREFIX_STATIC_RESOURCES + "/static/css/index.css";
+        EntityExchangeResult<String> response = client
+            .get()
+            .uri(uri)
+            .exchange()
+            .expectStatus()
+            .isOk()
+            .expectHeader()
+            .cacheControl(CacheControl.maxAge(Duration.ofDays(365)))
+            .expectBody(String.class)
+            .returnResult();
+
+        logger.debug("body : {}", response.getResponseBody());
+
+        String etag = response.getResponseHeaders()
+            .getETag();
+
+        client
+            .get()
+            .uri(uri)
+            .header("If-None-Match", etag)
+            .exchange()
+            .expectStatus()
+            .isNotModified();
+    }
+}
