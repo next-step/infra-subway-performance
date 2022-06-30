ed36583fcde94127921ae937116f83d2f9226446
toughchb <ggang2000@gmail.com>
2022-07-01 02:07:50 +0900

    fix: redis config disable

--- src/main/java/nextstep/subway/config/CacheConfig.java
+++ src/main/java/nextstep/subway/config/CacheConfig.java
@@ -1,5 +1,6 @@
 package nextstep.subway.config;
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.CachingConfigurerSupport;
 import org.springframework.context.annotation.Bean;
@@ -11,7 +12,7 @@ import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSeriali
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
-
+@EnableAutoConfiguration(exclude=CacheConfig.class)
 @Profile("prod")
 public class CacheConfig extends CachingConfigurerSupport {
 
