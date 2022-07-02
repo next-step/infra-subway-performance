a776c4fa4f890a1fd433c4c6280eb39696f503fa
otakjong <tj3828@naver.com>
2022-07-03 04:22:57 +0900

    feat: 미션 1 - 모든 정적 자원 no-cache, private

---
+++ src/test/java/nextstep/subway/StaticResourceTest.java
@@ -0,0 +1,16 @@
+package nextstep.subway;
+
+import io.restassured.RestAssured;
+import nextstep.subway.AcceptanceTest;
+import org.junit.jupiter.api.Test;
+
+public class StaticResourceTest extends AcceptanceTest {
+
+    @Test
+    void get_static_resources() {
+        RestAssured.given().log().all()
+            .when().get("/js/main.js")
+            .then().header("Cache-Control", "no-cache, private");
+    }
+
+}
