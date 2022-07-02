8a4fe5fd92b6f5890500e2b889de759c2ad7d0be
Sung-jin <osj4872@gmail.com>
2022-07-02 22:59:17 +0900

    chore: 모니터링 성능 측정 스크립트 추가

---
+++ monitoring/check/main_smoke.js
@@ -0,0 +1,19 @@
+import executor from '../../executor/index.js';
+
+const targetMethod = 'get';
+const targetPath = '/';
+const checkBy = {'response ok': (res) => res.status === 200}
+
+export const options = {
+    vus: 1, // 1 user looping for 1 minute
+    duration: '1m',
+
+    thresholds: {
+        http_req_duration: ['p(99)<1500'], // 99% of requests must complete below 1.5s
+    },
+
+};
+
+export default function() {
+    executor(targetMethod, targetPath, checkBy);
+}
---
+++ monitoring/check/main_stress.js
@@ -0,0 +1,20 @@
+import executor from '../../executor/index.js';
+
+const targetMethod = 'get';
+const targetPath = '/';
+const checkBy = {'response ok': (res) => res.status === 200}
+
+export const options = {
+    stages: [
+        { duration: '1m', target: 184 },
+        { duration: '10m', target: 184 },
+        { duration: '10s', target: 0 },
+    ],
+    thresholds: {
+        http_req_duration: ['p(99)<1500'], // 99% of requests must complete below 1.5s
+    },
+};
+
+export default function() {
+    executor(targetMethod, targetPath, checkBy);
+}
