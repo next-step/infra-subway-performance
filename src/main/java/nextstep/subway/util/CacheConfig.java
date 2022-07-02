dd2749693f102205e2cf2b06e10ee37c40596b3a
lcjltj <lcjltj@gmail.com>
2022-07-02 13:23:37 +0900

    fix: redis 캐시 prod 만 적용

--- src/main/java/nextstep/subway/util/CacheConfig.java
+++ src/main/java/nextstep/subway/util/CacheConfig.java
@@ -1,11 +1,11 @@
 package nextstep.subway.util;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.CachingConfigurerSupport;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -14,6 +14,7 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 import static org.springframework.data.redis.serializer.RedisSerializationContext.SerializationPair;
 
+@Profile("prod")
 @EnableCaching
 @Configuration
 public class CacheConfig extends CachingConfigurerSupport {
