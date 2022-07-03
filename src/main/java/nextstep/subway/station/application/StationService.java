4de4b6d20fe785dc9dbbe5305085764a13982726
tasklet1579 <tasklet1579@gmail.com>
2022-07-03 20:00:23 +0900

    add : 지하철역, 회원 로컬 캐시 적용

--- src/main/java/nextstep/subway/station/application/StationService.java
+++ src/main/java/nextstep/subway/station/application/StationService.java
@@ -4,6 +4,8 @@ import nextstep.subway.station.domain.Station;
 import nextstep.subway.station.domain.StationRepository;
 import nextstep.subway.station.dto.StationRequest;
 import nextstep.subway.station.dto.StationResponse;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -24,6 +26,9 @@ public class StationService {
         return StationResponse.of(persistStation);
     }
 
+    // 메서드 실행 전에 캐시를 확인하여 최소 하나의 캐시가 존재한다면 값을 반환한다.
+    // SpEL 표현식을 활용하여 조건부 캐싱이 가능하다.
+    @Cacheable(value = "station", unless = "#result == null")
     @Transactional(readOnly = true)
     public List<StationResponse> findAllStations() {
         List<Station> stations = stationRepository.findAll();
@@ -33,14 +38,18 @@ public class StationService {
                 .collect(Collectors.toList());
     }
 
+    // 캐시를 제거할 때 사용한다.
+    @CacheEvict(value = "line", key = "#id")
     public void deleteStationById(Long id) {
         stationRepository.deleteById(id);
     }
 
+    @Cacheable(value = "station", key = "#id", unless = "#result == null")
     public Station findStationById(Long id) {
         return stationRepository.findById(id).orElseThrow(RuntimeException::new);
     }
 
+    @Cacheable(value = "station", key = "#id", unless = "#result == null")
     public Station findById(Long id) {
         return stationRepository.findById(id).orElseThrow(RuntimeException::new);
     }
