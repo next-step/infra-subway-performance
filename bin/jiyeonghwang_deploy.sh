4cf78b97963b7a88531f43defcfd0db57c109b30
jiyeonghwang <gyp0288@naver.com>
2022-07-04 19:16:07 +0900

    refactor: deploy 파일 수정

--- bin/jiyeonghwang_deploy.sh
+++ bin/jiyeonghwang_deploy.sh
@@ -6,12 +6,20 @@ txtrst='\033[1;37m' # White
 txtylw='\033[1;33m' # Yellow
 txtgrn='\033[1;32m' # Green
 
-#REPOSITORY=/home/ubuntu/nextstep/infra-subway-deploy/
-REPOSITORY=/home/ubuntu/nextstep/infra-subway-performance
-
 echo -e "${txtylw}=======================================${txtrst}"
 echo -e "${txtgrn}  << 스크립트 🧐 >>${txtrst}"
 echo -e "${txtylw}=======================================${txtrst}"
+
+function clone() {
+
+## 저장소 clone
+    echo -e "${txtylw}=======================================${txtrst}"
+    echo -e "${txtgrn}<< Git Clone >>${txtrst}"
+    echo -e "${txtylw}=======================================${txtrst}"
+
+    git clone https://github.com/jiyeonghwang/infra-subway-performance
+    cd infra-subway-performance
+}
 
 function pull() {
 
@@ -20,10 +28,8 @@ function pull() {
     echo -e "${txtgrn}<< Git Pull >>${txtrst}"
     echo -e "${txtylw}=======================================${txtrst}"
 
-    cd $REPOSITORY
-    git pull
+    git pull origin step2
 }
-
 
 function build() {
 
@@ -31,47 +37,19 @@ function build() {
     echo -e "${txtylw}=======================================${txtrst}"
     echo -e "${txtgrn}<< build >>${txtrst}"
     echo -e "${txtylw}=======================================${txtrst}"
-        ./gradlew clean build
+
+    ./gradlew clean build
 
 }
-
-function search_pid() {
-
-## 프로세스 pid를 찾는 명령어
-    CURRENT_PID=$(pgrep -f subway-0.0.1-SNAPSHOT.jar)
-
-    echo -e "${txtylw}=======================================${txtrst}"
-    echo -e "pid: $CURRENT_PID"
-    echo -e "${txtylw}=======================================${txtrst}"
-
-}
-
-function process_kill() {
-
-## 프로세스를 종료하는 명령어
-    if [ -z $CURRENT_PID ]; then
-    echo -e "${txthlw} 현재 구동중인 어플리케이션이 없으므로 종료하지 않습니다."
-else
-    echo -e "${txtred} kill -2 $CURRENT_PID ${txtrst}"
-    kill -2 $CURRENT_PID
-    sleep 5
-    fi
-
-}
-
 
 function process_start() {
 
 ## 프로세스를 실행하는 명령어
-    cd $REPOSITORY/build/libs
-
     echo -e "${txtylw}=======================================${txtrst}"
     echo -e "프로세스 실행"
     echo -e "${txtylw}=======================================${txtrst}"
 
     nohup java -jar -Dspring.profiles.active=prod subway-0.0.1-SNAPSHOT.jar 1> log.log 2>&1 &
-
-    #java -jar -Dspring.profiles.active=prod subway-0.0.1-SNAPSHOT.jar &
 
     echo -e "${txtylw}=======================================${txtrst}"
     echo -e "프로세스 실행 완료"
@@ -79,8 +57,7 @@ function process_start() {
 
 }
 
+clone;
 pull;
 build;
-search_pid;
-process_kill;
 process_start;
