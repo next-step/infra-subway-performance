347b2d1aac5b8d87d0316c7d3d34282cf5147255
Donghee Han <handh0413@gmail.com>
2022-07-04 22:44:40 +0900

    feat: Spring Redis 관련 Profile 설정 변경

--- src/main/java/nextstep/subway/config/CacheConfig.java
+++ src/main/java/nextstep/subway/config/CacheConfig.java
@@ -3,8 +3,10 @@ package nextstep.subway.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -13,6 +15,8 @@ import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
+@EnableCaching
+@Profile("prod | local")
 public class CacheConfig extends CachingConfigurerSupport {
 
     @Autowired
