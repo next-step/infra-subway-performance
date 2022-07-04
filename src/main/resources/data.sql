23f51c8bcd2c7814b039bd0ae9a33e90064902e4
tasklet1579 <tasklet1579@gmail.com>
2022-07-04 15:01:46 +0900

    test : json 테스트

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,21 +1,21 @@
-insert into station(id, name, createdDate, modifiedDate) values (1, '강남역', now(), now());
-insert into station(id, name, createdDate, modifiedDate) values (2, '양재역', now(), now());
-insert into station(id, name, createdDate, modifiedDate) values (3, '판교역', now(), now());
-insert into station(id, name, createdDate, modifiedDate) values (4, '정자역', now(), now());
-insert into station(id, name, createdDate, modifiedDate) values (5, '미금역', now(), now());
-insert into station(id, name, createdDate, modifiedDate) values (6, '수지구청역', now(), now());
-insert into station(id, name, createdDate, modifiedDate) values (7, '성복역', now(), now());
-insert into station(id, name, createdDate, modifiedDate) values (8, '상현역', now(), now());
-insert into station(id, name, createdDate, modifiedDate) values (9, '광교중앙역', now(), now());
-insert into station(id, name, createdDate, modifiedDate) values (10, '성남역', now(), now());
-insert into station(id, name, createdDate, modifiedDate) values (11, '이매역', now(), now());
-insert into station(id, name, createdDate, modifiedDate) values (12, '광주역', now(), now());
-insert into station(id, name, createdDate, modifiedDate) values (13, '곤지암역', now(), now());
-insert into station(id, name, createdDate, modifiedDate) values (14, '이천역', now(), now());
-insert into station(id, name, createdDate, modifiedDate) values (15, '여주역', now(), now());
+insert into station(id, name, created_date, modified_date) values (1, '강남역', now(), now());
+insert into station(id, name, created_date, modified_date) values (2, '양재역', now(), now());
+insert into station(id, name, created_date, modified_date) values (3, '판교역', now(), now());
+insert into station(id, name, created_date, modified_date) values (4, '정자역', now(), now());
+insert into station(id, name, created_date, modified_date) values (5, '미금역', now(), now());
+insert into station(id, name, created_date, modified_date) values (6, '수지구청역', now(), now());
+insert into station(id, name, created_date, modified_date) values (7, '성복역', now(), now());
+insert into station(id, name, created_date, modified_date) values (8, '상현역', now(), now());
+insert into station(id, name, created_date, modified_date) values (9, '광교중앙역', now(), now());
+insert into station(id, name, created_date, modified_date) values (10, '성남역', now(), now());
+insert into station(id, name, created_date, modified_date) values (11, '이매역', now(), now());
+insert into station(id, name, created_date, modified_date) values (12, '광주역', now(), now());
+insert into station(id, name, created_date, modified_date) values (13, '곤지암역', now(), now());
+insert into station(id, name, created_date, modified_date) values (14, '이천역', now(), now());
+insert into station(id, name, created_date, modified_date) values (15, '여주역', now(), now());
 
-insert into line(id, name, color, createdDate, modifiedDate) values (1, '신분당선', 'yellow', now(), now());
-insert into line(id, name, color, createdDate, modifiedDate) values (2, '경강선', 'blue', now(), now());
+insert into line(id, name, color, created_date, modified_date) values (1, '신분당선', 'yellow', now(), now());
+insert into line(id, name, color, created_date, modified_date) values (2, '경강선', 'blue', now(), now());
 
 insert into section(id, line_id, up_station_id, down_station_id, distance) values (1, 1, 1, 2, 10);
 insert into section(id, line_id, up_station_id, down_station_id, distance) values (2, 1, 2, 3, 10);
@@ -32,4 +32,4 @@ insert into section(id, line_id, up_station_id, down_station_id, distance) value
 insert into section(id, line_id, up_station_id, down_station_id, distance) values (13, 2, 13, 14, 10);
 insert into section(id, line_id, up_station_id, down_station_id, distance) values (14, 2, 14, 15, 10);
 
-insert into member(id, email, password, age) values (1, 'tasklet1579@next.co.kr', 'test1234', 30);
+insert into member(id, email, password, age, created_date, modified_date) values (1, 'tasklet1579@next.co.kr', 'test1234', 30, now(), now());
