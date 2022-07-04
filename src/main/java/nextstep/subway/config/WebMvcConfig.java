c02939eb3cfdb13e064997ff47f0df520dbdcf97
Donghee Han <handh0413@gmail.com>
2022-07-05 00:25:34 +0900

    feat: Spring Boot에 HTTP 캐싱 적용

--- src/main/java/nextstep/subway/config/WebMvcConfig.java
+++ src/main/java/nextstep/subway/config/WebMvcConfig.java
@@ -5,11 +5,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
 import org.springframework.web.filter.ShallowEtagHeaderFilter;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.servlet.Filter;
+import java.util.concurrent.TimeUnit;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
@@ -20,9 +22,17 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler(PREFIX_STATIC_RESOURCES + "/" + version.getVersion() + "/**")
-                .addResourceLocations("classpath:/static/")
-                .setCachePeriod(60 * 60 * 24 * 365);
+        registry.addResourceHandler(PREFIX_STATIC_RESOURCES + "/" + version.getVersion() + "/css/**.css")
+                .addResourceLocations("classpath:/static/css/")
+                .setCacheControl(CacheControl.maxAge(365, TimeUnit.DAYS));
+
+        registry.addResourceHandler(PREFIX_STATIC_RESOURCES + "/" + version.getVersion() + "/js/**")
+                .addResourceLocations("classpath:/static/js/")
+                .setCacheControl(CacheControl.noCache().cachePrivate());
+
+        registry.addResourceHandler(PREFIX_STATIC_RESOURCES + "/" + version.getVersion() + "/images/**")
+                .addResourceLocations("classpath:/static/images/")
+                .setCacheControl(CacheControl.noCache().cachePrivate());
     }
 
     @Bean
---
+++ src/test/java/nextstep/subway/config/StaticResourcesTest.java
@@ -0,0 +1,99 @@
+package nextstep.subway.config;
+
+import nextstep.subway.support.version.SubwayVersion;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
+import org.springframework.http.CacheControl;
+import org.springframework.test.web.reactive.server.EntityExchangeResult;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import java.util.concurrent.TimeUnit;
+
+import static nextstep.subway.config.WebMvcConfig.PREFIX_STATIC_RESOURCES;
+
+@Profile("test")
+@AutoConfigureWebTestClient
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class StaticResourcesTest {
+    private static final Logger logger = LoggerFactory.getLogger(StaticResourcesTest.class);
+
+    @Autowired
+    private WebTestClient client;
+
+    @Autowired
+    private SubwayVersion version;
+
+    @Test
+    void get_javascript_resources() {
+        String uri = PREFIX_STATIC_RESOURCES + "/" + version.getVersion() + "/js/main.js";
+        EntityExchangeResult<String> response = client.get()
+                .uri(uri)
+                .exchange()
+                .expectStatus()
+                .isOk()
+                .expectHeader()
+                .cacheControl(CacheControl.noCache().cachePrivate())
+                .expectBody(String.class)
+                .returnResult();
+
+        String etag = response.getResponseHeaders().getETag();
+
+        client.get()
+                .uri(uri)
+                .header("If-None-Match", etag)
+                .exchange()
+                .expectStatus()
+                .isNotModified();
+    }
+
+    @Test
+    void get_css_resources() {
+        String uri = PREFIX_STATIC_RESOURCES + "/" + version.getVersion() + "/css/test.css";
+        EntityExchangeResult<String> response = client.get()
+                .uri(uri)
+                .exchange()
+                .expectStatus()
+                .isOk()
+                .expectHeader()
+                .cacheControl(CacheControl.maxAge(365, TimeUnit.DAYS))
+                .expectBody(String.class)
+                .returnResult();
+
+        String etag = response.getResponseHeaders().getETag();
+
+        client.get()
+                .uri(uri)
+                .header("If-None-Match", etag)
+                .exchange()
+                .expectStatus()
+                .isNotModified();
+    }
+
+    @Test
+    void get_static_resources() {
+        String uri = PREFIX_STATIC_RESOURCES + "/" + version.getVersion() + "/images/logo_small.png";
+        EntityExchangeResult<String> response = client.get()
+                .uri(uri)
+                .exchange()
+                .expectStatus()
+                .isOk()
+                .expectHeader()
+                .cacheControl(CacheControl.noCache().cachePrivate())
+                .expectBody(String.class)
+                .returnResult();
+
+        String etag = response.getResponseHeaders().getETag();
+
+        client.get()
+                .uri(uri)
+                .header("If-None-Match", etag)
+                .exchange()
+                .expectStatus()
+                .isNotModified();
+    }
+}
