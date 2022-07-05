cebe5b6fa4077ed6ba8c96cf2c828514f81d2355
tasklet1579 <tasklet1579@gmail.com>
2022-07-05 14:42:49 +0900

    fix : 지하철역 캐시 사용

--- src/main/java/nextstep/subway/station/application/StationService.java
+++ src/main/java/nextstep/subway/station/application/StationService.java
@@ -40,12 +40,10 @@ public class StationService {
         stationRepository.deleteById(id);
     }
 
-    @Cacheable(value = CacheConfig.STATION, key = "#id", unless = "#result == null")
     public Station findStationById(Long id) {
         return stationRepository.findById(id).orElseThrow(RuntimeException::new);
     }
 
-    @Cacheable(value = CacheConfig.STATION, key = "#id", unless = "#result == null")
     public Station findById(Long id) {
         return stationRepository.findById(id).orElseThrow(RuntimeException::new);
     }
