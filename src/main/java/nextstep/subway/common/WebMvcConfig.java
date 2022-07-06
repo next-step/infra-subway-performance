d1a283ed7f7f95da20d7e27d2ec8a3717c9f1cdf
minzzang <minjjang1117@gmail.com>
2022-07-06 12:30:20 +0900

    feat: springboot에 HTTP Cache, gzip 설정하기
    - 모든 정적 자원에 대해 no cache, no store 설정
    - js 자원에 대해 no-cache, private 설정
    - css 자원에 대해 max-age 1년 설정

---
+++ src/main/java/nextstep/subway/common/WebMvcConfig.java
@@ -0,0 +1,48 @@
+package nextstep.subway.common;
+
+import nextstep.subway.common.support.version.SubwayVersion;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
+import org.springframework.web.filter.ShallowEtagHeaderFilter;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.servlet.Filter;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+    public static final String PREFIX_STATIC_RESOURCES = "/resources";
+
+    @Autowired
+    private SubwayVersion version;
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        // 모든 정적 자원에 대해 no-cache, no-store 설정
+        registry.addResourceHandler(PREFIX_STATIC_RESOURCES + "/" + version.getVersion() + "/static/**")
+                .addResourceLocations("classpath:/static/")
+                .setCacheControl(CacheControl.noStore().mustRevalidate());
+
+        // css는 max-age를 1년으로 설정
+        registry.addResourceHandler(PREFIX_STATIC_RESOURCES + "/" + version.getVersion() + "/static/css/**")
+                .addResourceLocations("classpath:/static/css/")
+                .setCachePeriod(60 * 60 * 24 * 365);
+
+        // js는 no-cache, private 설정
+        registry.addResourceHandler(PREFIX_STATIC_RESOURCES + "/" + version.getVersion() + "/static/js/**")
+                .addResourceLocations("classpath:/static/js/")
+                .setCacheControl(CacheControl.noCache().cachePrivate());
+    }
+
+    @Bean
+    public FilterRegistrationBean filterRegistrationBean(){
+        FilterRegistrationBean registration = new FilterRegistrationBean();
+        Filter etagHeaderFilter = new ShallowEtagHeaderFilter();
+        registration.setFilter(etagHeaderFilter);
+        registration.addUrlPatterns(PREFIX_STATIC_RESOURCES + "/*");
+        return registration;
+    }
+}
