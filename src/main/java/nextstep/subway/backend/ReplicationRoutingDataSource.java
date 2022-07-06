b1389039a0dd1bdce78d6d7e5888282a903da853
jhsong2580 <thdwmdgns@naver.com>
2022-07-06 14:03:34 +0900

    feat: mysql replication config는 local/prod 환경에만 acvite된다

--- src/main/java/nextstep/subway/backend/ReplicationRoutingDataSource.java
+++ src/main/java/nextstep/subway/backend/ReplicationRoutingDataSource.java
@@ -1,8 +1,10 @@
 package nextstep.subway.backend;
 
+import org.springframework.context.annotation.Profile;
 import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
 import org.springframework.transaction.support.TransactionSynchronizationManager;
 
+@Profile({"local","prod"})
 public class ReplicationRoutingDataSource extends AbstractRoutingDataSource {
     public static final String DATASOURCE_KEY_MASTER = "master";
     public static final String DATASOURCE_KEY_SLAVE = "slave";
