ef3ba31829fea28eade3831fde0cfdeef5e9128e
Woody <yuncheol92@naver.com>
2022-07-06 21:45:35 +0900

    feat : cache 적용

--- src/main/java/nextstep/subway/line/application/LineService.java
+++ src/main/java/nextstep/subway/line/application/LineService.java
@@ -7,6 +7,9 @@ import nextstep.subway.line.dto.LineResponse;
 import nextstep.subway.line.dto.SectionRequest;
 import nextstep.subway.station.application.StationService;
 import nextstep.subway.station.domain.Station;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.Caching;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -24,6 +27,10 @@ public class LineService {
         this.stationService = stationService;
     }
 
+    @Caching(evict = {
+            @CacheEvict(value = "allLine", allEntries = true),
+            @CacheEvict(value = "path", allEntries = true)
+    })
     @Transactional
     public LineResponse saveLine(LineRequest request) {
         Station upStation = stationService.findById(request.getUpStationId());
@@ -32,6 +39,7 @@ public class LineService {
         return LineResponse.of(persistLine);
     }
 
+    @Cacheable(value = "allLine", unless = "#result.isEmpty()")
     public List<LineResponse> findLineResponses() {
         List<Line> persistLines = lineRepository.findAll();
         return persistLines.stream()
@@ -53,12 +61,20 @@ public class LineService {
         return LineResponse.of(persistLine);
     }
 
+    @Caching(evict = {
+            @CacheEvict(value = "allLine", allEntries = true),
+            @CacheEvict(value = "path", allEntries = true)
+    })
     @Transactional
     public void updateLine(Long id, LineRequest lineUpdateRequest) {
         Line persistLine = lineRepository.findById(id).orElseThrow(RuntimeException::new);
         persistLine.update(new Line(lineUpdateRequest.getName(), lineUpdateRequest.getColor()));
     }
 
+    @Caching(evict = {
+            @CacheEvict(value = "allLine", allEntries = true),
+            @CacheEvict(value = "path", allEntries = true)
+    })
     @Transactional
     public void deleteLineById(Long id) {
         lineRepository.deleteById(id);
