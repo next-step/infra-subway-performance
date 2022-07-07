0bb18165ce63ae0864e65c809a99840cffd6a157
minzzang <minjjang1117@gmail.com>
2022-07-07 15:55:47 +0900

    feat: Async 적용

--- src/main/java/nextstep/subway/member/application/MemberService.java
+++ src/main/java/nextstep/subway/member/application/MemberService.java
@@ -5,8 +5,8 @@ import nextstep.subway.member.domain.MemberRepository;
 import nextstep.subway.member.dto.MemberRequest;
 import nextstep.subway.member.dto.MemberResponse;
 import org.springframework.cache.annotation.CacheEvict;
-import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -29,11 +29,11 @@ public class MemberService {
         return memberRepository.findById(id).orElseThrow(RuntimeException::new);
     }
 
-    @CachePut(value = "member", key = "#id")
-    public Member updateMember(Long id, MemberRequest param) {
+    @CacheEvict(value = "member", key = "#id")
+    @Async
+    public void updateMember(Long id, MemberRequest param) {
         Member member = memberRepository.findById(id).orElseThrow(RuntimeException::new);
         member.update(param.toMember());
-        return member;
     }
 
     @CacheEvict(value = "member", key = "#id")
