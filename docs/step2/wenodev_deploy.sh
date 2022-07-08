40bf021515573417080ecf2d8bd4e09cd74135d8
wenodev <logicalstark1105@gmail.com>
2022-07-08 16:20:04 +0900

    fix: 배포 스크립트 변경

--- docs/step2/wenodev_deploy.sh
+++ docs/step2/wenodev_deploy.sh
@@ -1,65 +1,85 @@
 #!/bin/bash
 
 ## 변수 설정
-
+txtrst='\033[1;37m' # White
+txtred='\033[1;31m' # Red
 txtylw='\033[1;33m' # Yellow
 txtpur='\033[1;35m' # Purple
 txtgrn='\033[1;32m' # Green
+txtgra='\033[1;30m' # Gray
 
-BRANCH=$1
+BASE_DIR=/home/ubuntu/infra-subway-performance
+BRANCH=step2
+PROFILE=prod
 
-function deploy_message() {
-  echo -e "${txtylw}==============================================="
-  echo -e "${txtgrn}  << Deploy Start... >>${txtgrn}"
-  echo -e "${txtylw}==============================================="
-}
+echo -e "${txtylw}=======================================${txtrst}"
+echo -e "${txtgrn}       << deploy start >>${txtrst}"
+echo -e "${txtylw}=======================================${txtrst}"
 
+## git clone & checkout
 function clone() {
-    echo -e "git clone..."
-    git clone https://github.com/wenodev/infra-subway-performance
-    cd infra-subway-performance
-}
-
-function pull() {
-    echo -e ""
-    echo -e "${BRANCH} Pull Request ..."
-    git pull origin ${BRANCH}
-}
-
-function find_pid() {
   echo -e ""
-  echo -e ">> find PID 🏃..."
-  PID=`ps -ef | grep -v "grep" | jps | grep "subway" | awk '{print $1}'`
+  echo -e ">> Git Clone & Checkout"
+  git clone https://github.com/wenodev/infra-subway-performance.git
+  cd infra-subway-performance
+  git checkout ${BRANCH}
 }
 
-function kill_pid() {
-  if [ "$PID" != "" ]
-  then
-    echo -e ""
-    echo -e ">> Kill ${PID} 🏃..."
-    kill ${PID}
+## gradle build
+function build() {
+  echo -e ""
+  echo -e ">> Gradle Clean & Build"
+  ./gradlew clean build
+}
+
+## 프로세스 pid를 찾는 명령어
+function findPid() {
+  echo -e ""
+  echo -e ">> Find Process ID"
+  CURRENT_PID=$(pgrep -f subway)
+}
+
+
+## 프로세스를 종료하는 명령어
+function shutdownServer() {
+  echo -e ""
+  echo -e ">> Shutdown Server"
+  if [[ -n $CURRENT_PID ]]; then
+      kill $CURRENT_PID
+
+    echo -ne '## (20%)\r'
+    sleep 1
+    echo -ne '#### (40%)\r'
+    sleep 1
+    echo -ne '###### (60%)\r'
+    sleep 1
+    echo -ne '######## (80%)\r'
+    sleep 1
+    echo -ne '########## (100%)\r'
+    sleep 1
+
+    echo -e "Graceful Shutdown END <<"
   else
-    echo -e "PID is Empty!"
+    echo -e "NO Server EXIST <<"
   fi
 }
 
-function build() {
+## 프로세스를 시작하는 명령어
+function startServer() {
   echo -e ""
-  echo -e "${txtpur}>> Build ..."
-  ./gradlew clean build -x test
+  echo -e ">> Start Server"
+  nohup java -jar -Dspring.profiles.active=$PROFILE $BASE_DIR/build/libs/subway-0.0.1-SNAPSHOT.jar 1> $BASE_DIR/build/libs/log.out 2>&1 &
+
+  sleep 3
+
+  DEPLOY_PID=$(pgrep -f subway)
+  if [ DEPLOY_PID > 0 ]; then
+    echo -e "DEPLOY SUCCESS! <<"
+  fi
 }
 
-function run() {
-    echo -e " Application Run"
-    nohup java -jar -Dspring.profiles.active=prod build/libs/subway-0.0.1-SNAPSHOT.jar 1> /dev/null 2>&1  &
-}
-
-deploy_message
-clone
-pull
-build
-find_pid
-kill_pid
-run
-echo -e "Deploy success!"
-
+clone;
+build;
+findPid;
+shutdownServer;
+startServer;
