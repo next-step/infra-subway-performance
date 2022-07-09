17886425447b50cd9f87fbf00e572be51016231e
toughchb <ggang2000@gmail.com>
2022-07-10 02:30:55 +0900

    feat: station page 조회 추가

--- src/main/java/nextstep/subway/station/domain/StationRepository.java
+++ src/main/java/nextstep/subway/station/domain/StationRepository.java
@@ -1,10 +1,18 @@
 package nextstep.subway.station.domain;
 
+import nextstep.subway.station.dto.StationResponse;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface StationRepository extends JpaRepository<Station, Long> {
     @Override
     List<Station> findAll();
+
+    @Query("select s from Station s where s.id > :offset")
+    Slice<StationResponse> findAll(@Param("offset") Long offset, Pageable pageable );
 }
