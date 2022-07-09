4db95b15a7a86e4b53f17329a2a81cbbd2f8c83c
iamjunsulee <mellow0924@gmail.com>
2022-07-09 15:48:59 +0900

    refactor: 캐시 config 수정

--- src/main/java/nextstep/subway/config/CacheConfig.java
+++ src/main/java/nextstep/subway/config/CacheConfig.java
@@ -17,11 +17,8 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @EnableCaching
 @Configuration
 public class CacheConfig extends CachingConfigurerSupport {
-    private final RedisConnectionFactory connectionFactory;
-
-    public CacheConfig(RedisConnectionFactory connectionFactory) {
-        this.connectionFactory = connectionFactory;
-    }
+    @Autowired
+    RedisConnectionFactory connectionFactory;
 
     @Bean
     public CacheManager redisCacheManager() {
