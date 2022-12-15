1e9c932ecee875191c7b0b6cf05bd12c5634f947
CHEESE <81912261+ychxexn@users.noreply.github.com>
2022-12-15 23:51:02 +0900

    feat: ETag 적용

---
+++ src/main/java/nextstep/subway/config/SubwayHandlebarsHelper.java
@@ -0,0 +1,20 @@
+package nextstep.subway.config;
+
+import com.github.jknack.handlebars.Options;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import pl.allegro.tech.boot.autoconfigure.handlebars.HandlebarsHelper;
+
+@HandlebarsHelper
+public class SubwayHandlebarsHelper {
+    private static final Logger logger = LoggerFactory.getLogger(SubwayHandlebarsHelper.class);
+
+    @Autowired
+    private Version version;
+
+    public String staticUrls(String path, Options options) {
+        logger.debug("static url : {}", path);
+        return String.format("/resources/%s%s", version.getVersion(), path);
+    }
+}
