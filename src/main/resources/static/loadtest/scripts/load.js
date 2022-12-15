3cc50d6325e5e14a81bb46c62b9bfb8f93db7fde
jisu1211 <dbdnzl1211@gmail.com>
2022-12-16 04:28:43 +0900

    feat: 부하 테스트 스크립트 추가

---
+++ src/main/resources/static/loadtest/scripts/load.js
@@ -0,0 +1,52 @@
+import http from 'k6/http';
+import { check, group, sleep, fail } from 'k6';
+
+export let options = {
+    stages: [
+        { duration: '5m', target: 2 },
+        { duration: '5m', target: 5 },
+        { duration: '5m', target: 7 },
+        { duration: '5m', target: 7 },
+        { duration: '5m', target: 5 },
+        { duration: '5m', target: 2 }
+    ],
+    thresholds: {
+        http_req_duration: ['p(99)<100'], // 99% of requests must complete below 1.5s
+    },
+};
+
+const BASE_URL = 'https://jisu1211.kro.kr';
+const USERNAME = 'jisu1211';
+const PASSWORD = 'password157#';
+
+function main() {
+    check(http.get(`${BASE_URL}`), {
+        'You have successfully accessed the main page.': (res) => res.status === 200,
+    });
+}
+
+function path() {
+    check(http.get(`${BASE_URL}/path`), {
+        'You have successfully accessed the search path page.': (res) => res.status === 200,
+    });
+}
+
+function findPath() {
+    const params = {
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    };
+
+    check(http.get(`${BASE_URL}/path?source=1&target=100`, params), {
+        'The path was successfully searched.': (res) => res.status === 200,
+    });
+}
+
+export default function() {
+    main();
+    path();
+    findPath();
+
+    sleep(1);
+};
