90edd6147ae62a50062eea86b6ac529dfbf8d23e
CHEESE <81912261+ychxexn@users.noreply.github.com>
2022-12-16 00:24:14 +0900

    docs: 배포 스크립트 추가

---
+++ ychxexn-deploy.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+## 변수 설정
+txtrst='\033[1;37m' # White
+txtred='\033[1;31m' # Red
+txtylw='\033[1;33m' # Yellow
+txtpur='\033[1;35m' # Purple
+txtgrn='\033[1;32m' # Green
+txtgra='\033[1;30m' # Gray
+
+SUBWAY_PATH="/home/ubuntu/nextstep/infra-subway-performance"
+LOG_PATH="/home/ubuntu/logs/subway"
+BRANCH=$1
+PROFILE=$2
+
+echo -e "${txtylw}=======================================${txtrst}"
+echo -e "${txtgrn}  << 배포 스크립트 시작 >>${txtrst}"
+echo -e "${txtylw}=======================================${txtrst}"
+
+function clone() {
+  echo -e ""
+  echo -e ">> Git Clone"
+  git clone "https://github.com/ychxexn/infra-subway-performance.git"
+  cd $REPO
+}
+
+function pull() {
+  echo -e ""
+  echo -e ">> Git Pull Request ${BRANCH}"
+  git pull origin $BRANCH
+}
+
+function build() {
+  echo -e ""
+  echo -e ">> Build"
+  ./gradlew clean build
+}
+
+function killProcess() {
+  echo -e ""
+  echo -e ">> Kill Process 🏃️"
+  CURRENT_PID=$(pgrep -f subway)
+  if [[ $CURRENT_PID -gt 0 ]]; then
+    echo -e "kill $CURRENT_PID"
+    kill -2 $CURRENT_PID
+  fi
+}
+
+function deploy() {
+  echo -e ""
+  echo -e "${txtylw}>> deploy${txtrst}"
+  JAR_PATH=$(find $SUBWAY_PATH/build/libs/* -name "*.jar")
+  nohup java -jar -Dspring.profiles.active=$PROFILE $JAR_PATH 1> $LOG_PATH/out.log 2>&1  &
+}
+
+clone;
+pull;
+build;
+killProcess;
+deploy;
+
+echo -e "${txtylw}=======================================${txtrst}"
+echo -e "${txtgrn}  << 배포 스크립트 종료 >>${txtrst}"
+echo -e "${txtylw}=======================================${txtrst}"
+
+exit 0
