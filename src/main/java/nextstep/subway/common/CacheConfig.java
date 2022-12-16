47c4130ae3e3fcdbace87d55823edbdef0fca384
daegyeom.kim <daegyeom.kim@qcells.com>
2022-12-16 19:56:26 +0900

    feat(cacheConfig): ttl 설정 추가

--- src/main/java/nextstep/subway/common/CacheConfig.java
+++ src/main/java/nextstep/subway/common/CacheConfig.java
@@ -12,6 +12,8 @@ import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSeriali
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
+import java.time.Duration;
+
 @EnableCaching
 @Configuration
 public class CacheConfig extends CachingConfigurerSupport {
@@ -26,7 +28,8 @@ public class CacheConfig extends CachingConfigurerSupport {
     public CacheManager redisCacheManager() {
         RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
                 .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
-                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()));
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()))
+                .entryTtl(Duration.ofMinutes(1L));
 
         return RedisCacheManager.RedisCacheManagerBuilder.
                 fromConnectionFactory(redisConnectionFactory)
