6362c963914f5f92a9335ba7ea58dc273e6d1def
jisu1211 <dbdnzl1211@gmail.com>
2022-12-16 23:05:20 +0900

    refactor: 테스트 환경을 제외하고 Cache가 적용되도록 수정

--- src/main/java/nextstep/subway/configuration/CacheConfig.java
+++ src/main/java/nextstep/subway/configuration/CacheConfig.java
@@ -19,7 +19,7 @@ import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSeriali
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
-@Profile("prod")
+@Profile("!test")
 @EnableCaching
 @Configuration
 public class CacheConfig extends CachingConfigurerSupport {
