6d17eb61a81edd970036ec7d80012e13d8186030
chosundeveloper <67059660+chosundeveloper@users.noreply.github.com>
2022-12-17 12:10:13 +0900

    🚀 1단계 - 화면 응답 개선하기 (#414)
    * feat(somke_k6_before.png) : 성능 개선 결과 공유
    
    * docs(README.md) : 완료 요구사항 체크
    
    * feat(LineService.java) : WAS 캐싱 적용
    
    * feat(.gitmodules) : submodule 추가
    
    * docs(README.md) : 완료 요구사항 체크
    
    * refactor(smoke_k6_before.png) : 사진 크기 조절
    
    * docs(README.md) : url 추가

--- src/main/java/nextstep/subway/station/dto/StationResponse.java
+++ src/main/java/nextstep/subway/station/dto/StationResponse.java
@@ -1,5 +1,9 @@
 package nextstep.subway.station.dto;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import nextstep.subway.station.domain.Station;
 
 import java.time.LocalDateTime;
@@ -7,12 +11,12 @@ import java.time.LocalDateTime;
 public class StationResponse {
     private Long id;
     private String name;
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
     private LocalDateTime createdDate;
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
     private LocalDateTime modifiedDate;
-
-    public static StationResponse of(Station station) {
-        return new StationResponse(station.getId(), station.getName(), station.getCreatedDate(), station.getModifiedDate());
-    }
 
     public StationResponse() {
     }
@@ -22,6 +26,10 @@ public class StationResponse {
         this.name = name;
         this.createdDate = createdDate;
         this.modifiedDate = modifiedDate;
+    }
+
+    public static StationResponse of(Station station) {
+        return new StationResponse(station.getId(), station.getName(), station.getCreatedDate(), station.getModifiedDate());
     }
 
     public Long getId() {
