1c9dc9ad644c693b11ecb20348e5dfe2c1bee31d
namhyun.sung <namhyun.sung@yanolja.com>
2022-12-19 01:59:11 +0900

    feat: k6 test 작성

---
+++ k6script/stress.js
@@ -0,0 +1,86 @@
+import http from 'k6/http';
+import { check, group, sleep, fail } from 'k6';
+
+export let options = {
+  stages: [
+    { duration: '1m', target: 75 },
+    { duration: '1m', target: 200 },
+    { duration: '1m', target: 350 },
+    { duration: '1m', target: 500 },
+    { duration: '1m', target: 650 },
+    { duration: '1m', target: 800 },
+    { duration: '1m', target: 0 },
+  ],
+  thresholds: {
+    http_req_duration: ['p(99)<100'], // 99% of requests must complete below 0.1s
+  },
+};
+
+const BASE_URL = 'https://snh15978.p-e.kr/';
+const USERNAME = 'test@gmail.com';
+const PASSWORD = 'test1234!';
+
+
+export default function ()  {
+  mainPage();
+  const authHeaders = login();
+  retrieveMember(authHeaders);
+  pathPage();
+  findPath(authHeaders);
+
+  sleep(1);
+}
+
+
+function mainPage() {
+  const response = http.get(BASE_URL);
+
+  check(response, {
+    'entered in main page successfully': (res) => res.status === 200
+  });
+}
+
+function login() {
+  const payload = JSON.stringify({
+    email: USERNAME,
+    password: PASSWORD,
+  });
+  const params = {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
+
+  const response = http.post(`${BASE_URL}/login/token`, payload, params);
+
+  check(response, {
+    'logged in successfully': (res) => res.json('accessToken') !== '',
+  });
+
+  return {
+    headers: {
+      Authorization: `Bearer ${response.json('accessToken')}`,
+    },
+  };
+}
+
+function retrieveMember(authHeaders) {
+  let myObjects = http.get(`${BASE_URL}/members/me`, authHeaders).json();
+  check(myObjects, { 'retrieved member': (obj) => obj.id != 0 });
+}
+
+function pathPage() {
+  const response = http.get(`${BASE_URL}/path`);
+
+  check(response, {
+    'entered in path page successfully': (res) => res.status === 200
+  });
+}
+
+function findPath(authHeaders) {
+  const response = http.get(`${BASE_URL}/path?source=3&target=4`, authHeaders);
+
+  check(response, {
+    'get path info successfully': (res) => res.status === 200
+  });
+}
