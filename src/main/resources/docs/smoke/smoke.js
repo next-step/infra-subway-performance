da1fd0e37b6d18b44a3786f1ed848e2d6cc02ebe
sixthou <justyuk@naver.com>
2022-12-19 01:42:29 +0900

    docs : 리버스프록시 개선 사항 적용

---
+++ src/main/resources/docs/smoke/smoke.js
@@ -0,0 +1,85 @@
+/* smoke.js */
+import http from 'k6/http';
+import {check, group, sleep, fail} from 'k6';
+
+export let options = {
+    vus: 2,
+    duration: '1m',
+    thresholds: {
+        http_req_duration: ['p(99)<100'], // 99% of requests must complete below 0.1s
+    },
+};
+
+
+const BASE_URL = 'https://subway.sixthou.kro.kr';
+const USERNAME = 'test@test.com';
+const PASSWORD = '1q2w3e!@';
+
+export default function () {
+    accessMainPage();
+    accessLoginPage();
+    let accessToken = requestLogin();
+    accessPathPage();
+    findPath(accessToken);
+    sleep(1);
+};
+
+function accessMainPage() {
+    let mainPage = http.get(`${BASE_URL}`);
+    check(mainPage, {
+        'access main page': (res) => res.status === 200
+    });
+};
+
+function accessLoginPage() {
+    let loginPage = http.get(`${BASE_URL}/login`);
+    check(loginPage, {
+        'access login page': (res) => res.status === 200
+    });
+};
+
+function requestLogin() {
+
+    let payload = JSON.stringify({
+        email: USERNAME,
+        password: PASSWORD,
+    });
+
+    let params = {
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    };
+
+
+    let loginRes = http.post(`${BASE_URL}/login/token`, payload, params);
+
+    check(loginRes, {
+        'logged in successfully': (resp) => resp.json('accessToken') !== '',
+    });
+
+    return loginRes.json('accessToken');
+};
+
+function accessPathPage() {
+    let pathPage = http.get(`${BASE_URL}/path`);
+    check(pathPage, {
+        'access path page': (res) => res.status === 200
+    });
+};
+
+function findPath(accessToken) {
+
+    let authHeaders = {
+        headers: {
+            Authorization: `Bearer ${accessToken}`,
+        },
+    };
+    let myObjects = http.get(`${BASE_URL}/paths/?source=2&target=1`, authHeaders).json();
+    check(myObjects, {
+        'find path': (obj) => obj.stations.length !== 0
+    });
+    sleep(1);
+
+};
+
