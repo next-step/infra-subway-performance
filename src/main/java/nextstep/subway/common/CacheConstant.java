11754163ae0be71075621d68a7638e4c000890fc
sawhong <sawhong@gmarket.com>
2022-12-18 09:20:45 +0900

    feat: caching TTL 추가

--- src/main/java/nextstep/subway/common/CacheConstant.java
+++ src/main/java/nextstep/subway/common/CacheConstant.java
@@ -5,4 +5,5 @@ public class CacheConstant {
     public static final String lines = "lines";
     public static final String path = "path";
     public static final String stations = "stations";
+    public static final Long cacheTTLByDay = 1L;
 }
