72bb7d1bf1942841b1f84cc2dac4c999adac8f11
seogineer <dgseo8981@gmail.com>
2022-12-20 14:27:08 +0900

    feat: 모든 정적 자원에 대해 no-cache, private 설정을 하고 테스트 코드를 통해 검증

---
+++ src/test/java/nextstep/subway/StaticResourcesTest.java
@@ -0,0 +1,136 @@
+package nextstep.subway;
+
+import static nextstep.subway.cache.WebMvcConfig.PREFIX_STATIC_RESOURCES;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.CacheControl;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.reactive.server.EntityExchangeResult;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@ActiveProfiles("test")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class StaticResourcesTest {
+    private static final Logger logger = LoggerFactory.getLogger(StaticResourcesTest.class);
+
+    @Autowired
+    private WebTestClient client;
+
+    @Test
+    void get_static_resources_main() {
+        String uri = PREFIX_STATIC_RESOURCES + "/static/js/main.js";
+        EntityExchangeResult<String> response = client
+                .get()
+                .uri(uri)
+                .exchange()
+                .expectStatus()
+                .isOk()
+                .expectHeader()
+                .cacheControl(CacheControl.noCache().cachePrivate())
+                .expectBody(String.class)
+                .returnResult();
+
+        logger.debug("body : {}", response.getResponseBody());
+
+        String etag = response.getResponseHeaders()
+                .getETag();
+
+        client
+                .get()
+                .uri(uri)
+                .header("If-None-Match", etag)
+                .exchange()
+                .expectStatus()
+                .isNotModified();
+    }
+
+    @Disabled("Exceeded limit on max bytes to buffer")
+    @Test
+    void get_static_resources_vendors() {
+        String uri = PREFIX_STATIC_RESOURCES + "/static/js/vendors.js";
+        EntityExchangeResult<String> response = client
+                .get()
+                .uri(uri)
+                .exchange()
+                .expectStatus()
+                .isOk()
+                .expectHeader()
+                .cacheControl(CacheControl.noCache().cachePrivate())
+                .expectBody(String.class)
+                .returnResult();
+
+        logger.debug("body : {}", response.getResponseBody());
+
+        String etag = response.getResponseHeaders()
+                .getETag();
+
+        client
+                .get()
+                .uri(uri)
+                .header("If-None-Match", etag)
+                .exchange()
+                .expectStatus()
+                .isNotModified();
+    }
+
+    @Test
+    void get_static_resources_logo_small() {
+        String uri = PREFIX_STATIC_RESOURCES + "/static/images/logo_small.png";
+        EntityExchangeResult<String> response = client
+                .get()
+                .uri(uri)
+                .exchange()
+                .expectStatus()
+                .isOk()
+                .expectHeader()
+                .cacheControl(CacheControl.noCache().cachePrivate())
+                .expectBody(String.class)
+                .returnResult();
+
+        logger.debug("body : {}", response.getResponseBody());
+
+        String etag = response.getResponseHeaders()
+                .getETag();
+
+        client
+                .get()
+                .uri(uri)
+                .header("If-None-Match", etag)
+                .exchange()
+                .expectStatus()
+                .isNotModified();
+    }
+
+    @Test
+    void get_static_resources_main_logo() {
+        String uri = PREFIX_STATIC_RESOURCES + "/static/images/main_logo.png";
+        EntityExchangeResult<String> response = client
+                .get()
+                .uri(uri)
+                .exchange()
+                .expectStatus()
+                .isOk()
+                .expectHeader()
+                .cacheControl(CacheControl.noCache().cachePrivate())
+                .expectBody(String.class)
+                .returnResult();
+
+        logger.debug("body : {}", response.getResponseBody());
+
+        String etag = response.getResponseHeaders()
+                .getETag();
+
+        client
+                .get()
+                .uri(uri)
+                .header("If-None-Match", etag)
+                .exchange()
+                .expectStatus()
+                .isNotModified();
+    }
+}
