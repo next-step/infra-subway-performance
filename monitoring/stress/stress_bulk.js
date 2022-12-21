a9f1b0d3ea629a834825ac2ca0f9f1662e6d262c
SanghoonSon <mue0329@naver.com>
2022-12-21 23:25:29 +0900

    refactor: 스케일 아웃 정상 측정을 위한 테스트 스크립트 수정

--- monitoring/stress/stress_bulk.js
+++ monitoring/stress/stress_bulk.js
@@ -8,15 +8,15 @@ export let options = {
     { duration: '10s', target: 160 }, // ramping up
     { duration: '2m', target: 160 },
     { duration: '10s', target: 320 }, // ramping up
-    { duration: '3m', target: 320 },
+    { duration: '2m', target: 320 },
     { duration: '10s', target: 640 }, // ramping up
-    { duration: '4m', target: 640 },
+    { duration: '2m', target: 640 },
     { duration: '10s', target: 1280 }, // ramping up
     { duration: '4m', target: 1280 },
     { duration: '10s', target: 640 }, // ramping down
-    { duration: '4m', target: 640 },
+    { duration: '2m', target: 640 },
     { duration: '10s', target: 320 }, // ramping down
-    { duration: '3m', target: 320 },
+    { duration: '2m', target: 320 },
     { duration: '10s', target: 160 }, // ramping down
     { duration: '2m', target: 160 },
     { duration: '10s', target: 80 }, // ramping down
@@ -29,10 +29,15 @@ export let options = {
 };
 
 const BASE_URL = 'https://shshon-infra.o-r.kr';
+const USERNAME = 'test@test.com';
+const PASSWORD = '1234';
 
 export default function ()  {
-  // 메인 페이지 -> 경로 탐색 페이지 -> 경로 탐색 요청
+  // 메인 페이지 -> 로그인 페이지 -> 로그인 -> 경로 탐색 페이지 -> 경로 탐색 요청
   loadMainPage();
+  loadLoginPage();
+  let token = login();
+  completeLogin(token);
   loadPathPage();
   findPath();
   sleep(1);
@@ -45,6 +50,46 @@ function loadMainPage() {
   });
 }
 
+function loadLoginPage() {
+  let loginPageResponse = http.get(`${BASE_URL}/login`);
+  check(loginPageResponse, {
+    'loaded login page in successfully': (response) => response.status === 200,
+  });
+}
+
+function login() {
+  const payload = JSON.stringify({
+    email: USERNAME,
+    password: PASSWORD,
+  });
+
+  const params = {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
+
+  let loginResponse = http.post(`${BASE_URL}/login/token`, payload, params);
+  check(loginResponse, {
+    'Logged in successfully': (response) => response.json('accessToken') !== '',
+  });
+
+  return loginResponse.json('accessToken');
+}
+
+function completeLogin(accessToken) {
+  let authHeaders = {
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+  };
+
+  let completeLoginResponse = http.get(`${BASE_URL}/members/me`, authHeaders).json();
+  check(completeLoginResponse, {
+    'Complete logged in successfully': (response) => response.id !== 0,
+  });
+}
+
 function loadPathPage() {
   const response = http.get(`${BASE_URL}/path`);
   check(response, {
@@ -53,8 +98,8 @@ function loadPathPage() {
 }
 
 function findPath() {
-  let source = Math.floor(Math.random() * 10 + 1);
-  let target = Math.floor(Math.random() * 10 + 1);
+  let source = Math.floor(Math.random() * 15 + 1);
+  let target = Math.floor(Math.random() * 15 + 1);
   const response = http.get(`${BASE_URL}/paths?source=${source}&target=${target}`);
   check(response, {
     'get path info in successfully': (res) => res.status === 200,
