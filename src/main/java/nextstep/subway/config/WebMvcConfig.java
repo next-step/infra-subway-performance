3af33b374569274af45d1df202b2b9e86d14f27d
dval <ojsboy@hotmail.com>
2023-02-21 23:24:41 +0900

    [step3] 정적 리소스에 캐시 60초 적용

---
+++ src/main/java/nextstep/subway/config/WebMvcConfig.java
@@ -0,0 +1,21 @@
+package nextstep.subway.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.concurrent.TimeUnit;
+
+@Configuration
+@EnableWebMvc
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/static/")
+                .setCacheControl(CacheControl.maxAge(60, TimeUnit.SECONDS));
+    }
+}
